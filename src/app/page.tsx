7cbe7f9ae65e2f080ec584901df2527f430cd4f4
H-sooyeon <wndqhr__@naver.com>
2024-04-14 23:17:31 +0900

    feat: 홈 main 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import AgoraStatus from "./_components/home/atoms/AgoraStatus";
 import AgoraStatusTab from "./_components/home/atoms/AgoraStatusTab";
+import CategoryButton from "./_components/home/atoms/CategoryButton";
 import PageTitle from "./_components/home/atoms/PageTitle";
 import SearchBar from "./_components/home/atoms/SearchBar";
+import AgoraList from "./_components/home/molecules/AgoraList";
+import CategoryButtonList from "./_components/home/molecules/CategoryButtonList";
 
 export default function Home() {
   return (
-    <section className="flex flex-col flex-1 h-dvh md:pb-4rem">
+    <section className="flex flex-col flex-1 h-dvh max-lg:pb-4rem">
       <header className="p-1rem">
         <PageTitle
           title="아고라 검색"
@@ -15,8 +18,14 @@ export default function Home() {
         </PageTitle>
         <SearchBar />
       </header>
-      <main className="bg-yellow-400 justify-center items-stretch flex flex-1 flex-row h-dvh max-w-screen-2xl">
-        <AgoraStatusTab />
+      <main className="justify-center items-stretch flex flex-col h-fit flex-1 max-w-screen-2xl min-w-270">
+        <section className="w-full">
+          <AgoraStatusTab />
+          <CategoryButtonList />
+        </section>
+        <section className="flex flex-1 p-0.5rem pb-5rem pt-1rem justify-center items-start">
+          <AgoraList />
+        </section>
       </main>
     </section>
   );
