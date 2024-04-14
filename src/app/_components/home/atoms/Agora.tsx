5c56bfd60709c9f73d1bea66413f2feb11b2ce19
H-sooyeon <wndqhr__@naver.com>
2024-04-14 23:17:14 +0900

    feat: main 아고라 채팅방 추가

---
+++ src/app/_components/home/atoms/Agora.tsx
@@ -0,0 +1,25 @@
+export default function Agora() {
+  return (
+    <article className="w-7.5rem p-10 border-1 rounded-lg flex flex-col justify-center items-center">
+      <h3 className="text-sm pb-3">쌀떡보다 밀떡</h3>
+      <div className="w-3rem h-3rem rounded-full bg-yellow-400" />
+      <p className="text-xs pt-7">
+        <div className="pb-1">
+          <span className="pr-5 text-athens-gray-500 text-nowrap">
+            <span className="text-blue-500 pr-3">찬성</span>10명
+          </span>
+          <span className="text-athens-gray-500 text-nowrap">
+            <span className="text-red-500 pr-3">반대</span>10명
+          </span>
+        </div>
+        <span>
+          <span className="pr-3">관찰자</span>
+          <span className="text-athens-gray-500">10명</span>
+        </span>
+      </p>
+      <button className="text-xs text-white bg-athens-main p-4 pt-5 pb-5 mt-10 w-6rem rounded-md">
+        입장하기
+      </button>
+    </article>
+  );
+}
---
+++ src/app/_components/home/molecules/AgoraList.tsx
@@ -0,0 +1,31 @@
+import Agora from "../atoms/Agora";
+
+// mobile: '360px' // @media (min-width: 360px)
+// foldable: '523px' // @media (min-width: 523px)
+// tablet: '768px' // @media (min-width: 768px)
+// 'under-foldable': { max: '522px' } // @media (max-width: 522px)
+// 'under-tablet': { max: '767px' } // @media (max-width: 767px)
+// 'under-mobile': { max: '359px' } // @media (max-width: 359px)
+// 'sm': '640px' // @media (min-width: 640px)
+// 'md': '768px' // @media (min-width: 768px)
+// 'lg': '1024px' // @media (min-width: 1024px)
+// 'xl': '1280px' // @media (min-width: 1280px)
+// '2xl': '1536px' // @media (min-width: 1536px)
+
+export default function AgoraList() {
+  return (
+    <div className="grid under-large:grid-cols-7 gap-x-7 gap-y-10 under-mobile:grid-cols-2 mobile:grid-cols-3 foldable:grid-cols-4 tablet:grid-cols-6 under-tablet:grid-cols-5 xl:grid-cols-8 2xl:grid-cols-10  sm:grid-cols-4 lg:grid-cols-7 under-xl:grid-cols-6">
+      <Agora />
+      <Agora />
+      <Agora />
+      <Agora />
+      <Agora />
+      <Agora />
+      <Agora />
+      <Agora />
+      <Agora />
+      <Agora />
+      <Agora />
+    </div>
+  );
+}
