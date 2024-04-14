e52dbbe16cf975bdb7f6ef34c2c477fe3915de72
H-sooyeon <wndqhr__@naver.com>
2024-04-14 23:15:56 +0900

    feat: category 버튼 추가

---
+++ src/app/_components/home/atoms/CategoryButton.tsx
@@ -0,0 +1,16 @@
+type Props = {
+  innerText: string;
+  isActive: boolean;
+};
+
+export default function CategoryButton({ innerText, isActive }: Props) {
+  return (
+    <button
+      className={`${
+        isActive ? "bg-athens-sub" : "bg-athens-gray"
+      } flex justify-center p-10 pl-1.5rem pr-1.5rem rounded-full text-xs ml-5`}
+    >
+      {innerText}
+    </button>
+  );
+}
---
+++ src/app/_components/home/molecules/CategoryButtonList.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import CategoryButton from "../atoms/CategoryButton";
+// import { register, SwiperContainer, SwiperSlide } from "swiper/element/bundle";
+// register();
+
+const categoryList = [
+  { innerText: "전체", isActive: true },
+  { innerText: "지식/공부", isActive: false },
+  { innerText: "문화/예술", isActive: false },
+  { innerText: "음식/여행", isActive: false },
+  { innerText: "일상/취미", isActive: false },
+];
+
+// TODO: swiper 적용 혹은 다른 라이브러리 찾아보기
+export default function CategoryButtonList() {
+  return (
+    <div className="mt-10 flex text-nowrap overflow-hidden ml-5">
+      {categoryList.map((category) => (
+        <CategoryButton
+          key={category.innerText}
+          innerText={category.innerText}
+          isActive={category.isActive}
+        />
+      ))}
+    </div>
+  );
+}
