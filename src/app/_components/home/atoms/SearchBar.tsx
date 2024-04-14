046b2cb9f53e56c6fbeeaee1b380366f9d344fb7
H-sooyeon <wndqhr__@naver.com>
2024-04-14 18:02:56 +0900

    feat: 검색 컴포넌트 추가

--- src/app/_components/home/atoms/SearchBar.tsx
+++ src/app/_components/home/atoms/SearchBar.tsx
@@ -8,11 +8,14 @@ export default function SearchBar() {
       <SearchIcon className="w-1rem ml-10" />
       <input
         type="text"
-        className="w-full bg-athens-gray border-0 focus:outline-none pl-1rem placeholder:font-normal"
+        className="w-full text-sm bg-athens-gray border-0 focus:outline-none pl-1rem placeholder:font-normal"
         placeholder="검색"
       />
       <div className="flex justify-center items-center w-1.5rem h-1.5rem">
-        <RemoveIcon className="w-10" />
+        <RemoveIcon
+          className="w-10"
+          aria-label="입력한 검색 텍스트 전체 삭제 버튼"
+        />
       </div>
     </div>
   );
