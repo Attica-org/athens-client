8036b856c9dd81b729e8f9a20a29fe16de97e65f
H-sooyeon <wndqhr__@naver.com>
2024-04-14 23:15:27 +0900

    design: margin padding으로 변경

--- src/app/_components/home/atoms/AgoraStatusTab.tsx
+++ src/app/_components/home/atoms/AgoraStatusTab.tsx
@@ -2,7 +2,7 @@ export default function AgoraStatusTab() {
   // const state: "active" | "ended" = "active";
 
   return (
-    <nav className="flex flex-row justify-around items-center h-2rem w-full text-xs ml-5 mr-5">
+    <nav className="flex flex-row justify-around items-center h-2rem w-full text-xs pl-5 pr-5">
       <button className="border-b-1 border-athens-sub flex flex-1 justify-center p-6">
         활성화
       </button>
