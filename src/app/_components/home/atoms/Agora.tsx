0bf07673f3ab3e785f957a30a47e7214df587cb1
H-sooyeon <wndqhr__@naver.com>
2024-04-14 23:23:21 +0900

    feat: 아고라 대기|진행 상태 추가

--- src/app/_components/home/atoms/Agora.tsx
+++ src/app/_components/home/atoms/Agora.tsx
@@ -2,7 +2,9 @@ export default function Agora() {
   return (
     <article className="w-7.5rem p-10 border-1 rounded-lg flex flex-col justify-center items-center">
       <h3 className="text-sm pb-3">쌀떡보다 밀떡</h3>
-      <div className="w-3rem h-3rem rounded-full bg-yellow-400" />
+      <div className="w-3rem h-3rem rounded-full bg-yellow-400 relative">
+        <div className="w-0.5rem h-0.5rem rounded-full bg-red-400 absolute top-2 right-3 z-10" />
+      </div>
       <p className="text-xs pt-7">
         <div className="pb-1">
           <span className="pr-5 text-athens-gray-500 text-nowrap">
