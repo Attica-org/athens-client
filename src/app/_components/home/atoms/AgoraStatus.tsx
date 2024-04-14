4e6a5929ad3db0a1f9ba77297bc7e03c694664cd
H-sooyeon <wndqhr__@naver.com>
2024-04-14 18:03:56 +0900

    feat: 아고라 채팅 활성 선택 버튼 추가

--- src/app/_components/home/atoms/AgoraStatus.tsx
+++ src/app/_components/home/atoms/AgoraStatus.tsx
@@ -1,7 +1,7 @@
 export default function AgoraStatus() {
   return (
     <div className="flex flex-col justify-start items-start p-10">
-      <div className="flex items-center text-xs text-gray-600">
+      <div className="flex items-center text-xs text-gray-600 whitespace-nowrap">
         <div className="bg-red-400 w-8 h-8 rounded-full mr-10" />
         대기중
       </div>
--- src/app/_components/home/atoms/AgoraStatusTab.tsx
+++ src/app/_components/home/atoms/AgoraStatusTab.tsx
@@ -1,8 +1,14 @@
 export default function AgoraStatusTab() {
-  const state: "active" | "ended" = "active";
+  // const state: "active" | "ended" = "active";
+
   return (
-    <div>
-      아고라 상태 선택 탭<div>탭</div>
-    </div>
+    <nav className="flex flex-row justify-around items-center h-2rem w-full text-xs ml-5 mr-5">
+      <button className="border-b-1 border-athens-sub flex flex-1 justify-center p-6">
+        활성화
+      </button>
+      <button className="flex flex-1 justify-center p-6 border-b-1">
+        종료
+      </button>
+    </nav>
   );
 }
