6cc24d56057dbbcc26d62e7909985b6e4b2022c8
H-sooyeon <wndqhr__@naver.com>
2024-04-17 18:05:32 +0900

    feat: 모달 기본 베이스 코드 추출

---
+++ src/app/_components/home/molecules/ModalBase.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { MouseEventHandler, useEffect } from "react";
+import BackButton from "../../BackButton";
+import { useRouter } from "next/navigation";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function ModalBase({ children }: Props) {
+  const router = useRouter();
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+    return () => {
+      document.body.style.overflow = "unset";
+    };
+  }, []);
+
+  const clickOutSideModal: MouseEventHandler<HTMLDivElement> = (e) => {
+    if (e.target === e.currentTarget) router.back();
+  };
+
+  return (
+    <div
+      onClick={clickOutSideModal}
+      className="w-dvw h-dvh flex absolute justify-center items-center z-20 top-0 right-0 left-0 bottom-0 bg-opacity-20 bg-gray-600"
+    >
+      <div className="bg-white bottom-7rem mobile:w-[70vw] under-mobile:w-[80vw] w-[50vw] lg:w-40rem h-[27vh] under-mobile:h-[26vh] relative rounded-2xl min-w-220">
+        <BackButton className="absolute right-2rem top-25" />
+        {children}
+      </div>
+    </div>
+  );
+}
