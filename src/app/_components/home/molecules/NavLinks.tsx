9a7afd2e9a49af0cd9003dc479761c9408c2c294
H-sooyeon <wndqhr__@naver.com>
2024-04-20 11:42:23 +0900

    feat: 활성화 경로 찾기 기능 아이콘으로 변경으로 인한 코드 prop 제거

--- src/app/_components/home/molecules/NavLinks.tsx
+++ src/app/_components/home/molecules/NavLinks.tsx
@@ -1,29 +1,18 @@
-"use client";
-
 import HomeIcon from "@/assets/icons/homeIcon";
 import AddIcon from "@/assets/icons/addIcon";
-import { useSelectedLayoutSegment } from "next/navigation";
 import MobileNavLink from "../atoms/MobileNavLink";
 import DesktopNavLink from "../atoms/DesktopNavLink";
 
 export default function NavLinks() {
-  const pathname = useSelectedLayoutSegment();
-  // 전역 상태 관리 훅을 사용하여 pathname을 가져온다.
-  // create-agora 페이지에서는 전역 상태를 create-agora로 설정하고, 그 외의 페이지에서는 home으로 설정한다.
-
   return (
     <>
       <div className="hidden lg:block">
         <ul>
-          <DesktopNavLink href="/" segment={pathname} innerText="홈">
-            <HomeIcon className="w-1.5rem" segment={pathname} />
+          <DesktopNavLink href="/" innerText="홈">
+            <HomeIcon className="w-1.5rem" />
           </DesktopNavLink>
-          <DesktopNavLink
-            href="/flow/create-agora"
-            segment={`/${pathname}`}
-            innerText="아고라 생성"
-          >
-            <AddIcon className="w-1.5rem" segment={pathname} />
+          <DesktopNavLink href="/create-agora" innerText="아고라 생성">
+            <AddIcon className="w-1.5rem" />
           </DesktopNavLink>
         </ul>
       </div>
@@ -33,13 +22,13 @@ export default function NavLinks() {
             className="flex p-5 text-xs flex-col justify-center items-center"
             href="/"
           >
-            <HomeIcon className="w-23 pb-2" segment={pathname} />홈
+            <HomeIcon className="w-23 pb-2" />홈
           </MobileNavLink>
           <MobileNavLink
             className="flex flex-col text-xs justify-center items-center p-10"
-            href="/flow/create-agora"
+            href="/create-agora"
           >
-            <AddIcon className="w-23 pb-2" segment={pathname} />
+            <AddIcon className="w-23 pb-2" />
             아고라생성
           </MobileNavLink>
         </ul>
