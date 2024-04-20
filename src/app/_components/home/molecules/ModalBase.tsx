51140ea9eaaa1a6a866939d6d0403a1a32e328c0
H-sooyeon <wndqhr__@naver.com>
2024-04-20 11:41:43 +0900

    design: 모바일 화면에서의 margin, padding 값 추가

--- src/app/_components/home/molecules/ModalBase.tsx
+++ src/app/_components/home/molecules/ModalBase.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { MouseEventHandler, useEffect } from "react";
-import BackButton from "../../BackButton";
+import BackButton from "../atoms/BackButton";
 import { useRouter } from "next/navigation";
 
 type Props = {
@@ -27,7 +27,7 @@ export default function ModalBase({ children }: Props) {
       onClick={clickOutSideModal}
       className="w-dvw h-dvh flex absolute justify-center items-center z-20 top-0 right-0 left-0 bottom-0 bg-opacity-20 bg-gray-600"
     >
-      <div className="bg-white bottom-7rem mobile:w-[70vw] under-mobile:w-[80vw] w-[50vw] lg:w-40rem h-[27vh] under-mobile:h-[26vh] relative rounded-2xl min-w-220">
+      <div className="mt-10rem under-mobile:mt-14rem bg-white bottom-7rem mobile:w-[70vw] pb-2rem under-mobile:pb-1rem under-mobile:w-[80vw] w-[50vw] lg:w-40rem relative rounded-2xl min-w-220">
         <BackButton className="absolute right-2rem top-25" />
         {children}
       </div>
