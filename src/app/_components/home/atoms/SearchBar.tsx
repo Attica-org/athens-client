bd1f35d5cf114c51903370c30ea7d0abce4375b5
H-sooyeon <wndqhr__@naver.com>
2024-04-21 15:19:57 +0900

    feat: 검색 창 상태 저장 및 x 아이콘 클릭 시 상태 삭제

--- src/app/_components/home/atoms/SearchBar.tsx
+++ src/app/_components/home/atoms/SearchBar.tsx
@@ -1,8 +1,20 @@
+"use client";
+
 import RemoveIcon from "@/assets/icons/RemoveIcon";
 import SearchIcon from "@/assets/icons/SearchIcon";
+import { ChangeEventHandler, useState } from "react";
 
 export default function SearchBar() {
-  // TODO: 삭제 버튼 클릭 시 input 내용 삭제
+  const [inputText, setInputText] = useState<string>("");
+
+  const removeInputText = () => {
+    setInputText("");
+  };
+
+  const changeInputText: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setInputText(e.target.value);
+  };
+
   return (
     <div className="bg-athens-gray rounded-md p-4 flex justify-center items-center">
       <SearchIcon className="w-1rem ml-10" />
@@ -10,11 +22,14 @@ export default function SearchBar() {
         type="text"
         className="w-full text-sm bg-athens-gray border-0 focus:outline-none pl-1rem placeholder:font-normal"
         placeholder="검색"
+        value={inputText}
+        onChange={changeInputText}
       />
       <div className="flex justify-center items-center w-1.5rem h-1.5rem">
         <RemoveIcon
-          className="w-10"
+          className="w-10 cursor-pointer"
           aria-label="입력한 검색 텍스트 전체 삭제 버튼"
+          onClick={removeInputText}
         />
       </div>
     </div>
