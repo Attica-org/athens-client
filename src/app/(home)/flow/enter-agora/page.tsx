a02888547324e2ef54de6a7913d3eb3980be5f16
H-sooyeon <wndqhr__@naver.com>
2024-04-21 15:19:36 +0900

    feat: 새로 고침 시 배경 사라짐 수정

--- src/app/(home)/flow/enter-agora/page.tsx
+++ src/app/(home)/flow/enter-agora/page.tsx
@@ -1,5 +1,11 @@
 import EnterAgora from "@/app/_components/home/template/EnterAgora";
+import Main from "@/app/_components/home/template/Main";
 
 export default function Page() {
-  return <EnterAgora />;
+  return (
+    <>
+      <Main />
+      <EnterAgora />
+    </>
+  );
 }
