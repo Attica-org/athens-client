29115914bb52c66ce8e388e27a9b4e75e195d6a8
H-sooyeon <wndqhr__@naver.com>
2024-04-21 23:30:19 +0900

    feat: 뒤로가기 아이콘 추가

---
+++ src/assets/icons/BackIcon.tsx
@@ -0,0 +1,18 @@
+type Props = {
+  className: string;
+  onClick: () => void;
+};
+
+export default function BackIcon({ className, onClick }: Props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 -960 960 960"
+      className={className}
+      onClick={onClick}
+      aria-hidden
+    >
+      <path d="M400-80 0-480l400-400 56 57-343 343 343 343-56 57Z" />
+    </svg>
+  );
+}
