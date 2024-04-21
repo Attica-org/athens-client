122173b947d5d523c7885bfad819860625e36d59
H-sooyeon <wndqhr__@naver.com>
2024-04-21 23:27:43 +0900

    feat: 채팅방 헤더 추가

---
+++ src/app/_components/chat/molecules/AgoraTitle.tsx
@@ -0,0 +1,16 @@
+import SpeakerIcon from "@/assets/icons/SpeakerIcon";
+
+export default function AgoraTitle() {
+  return (
+    <div className="flex flex-col justify-center items-center w-full">
+      <p className="text-sm font-semibold flex justify-center items-center p-5">
+        <SpeakerIcon className="w-18 mr-0.5rem" fill="#10AE5D" />
+        국가 발전에는 유능한 독재자가 필요한 시기가 있다.
+      </p>
+      <div className="flex justify-around items-center w-full text-xs p-6">
+        <div className="text-blue-400">찬성 10</div>
+        <div className="text-red-400">반대 10</div>
+      </div>
+    </div>
+  );
+}
