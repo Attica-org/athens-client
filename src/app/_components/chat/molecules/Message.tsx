ea9683896882ed447334e266baeb217f42d4fd26
H-sooyeon <wndqhr__@naver.com>
2024-04-22 20:11:20 +0900

    feat: 메시지 리스트로 데이터화

--- src/app/_components/chat/molecules/Message.tsx
+++ src/app/_components/chat/molecules/Message.tsx
@@ -1,33 +1,46 @@
 "use client";
-import Image from "next/image";
-import { useState } from "react";
 import MyMessage from "../atoms/MyMessage";
 import YourMessage from "../atoms/YourMessage";
-
-type Message = {
-  role: string;
-  message: string;
-  name: string;
-  createdAt: string;
-};
+import { Message as IMessage } from "@/app/model/Message";
 
 export default function Message() {
-  const myRole = "pro";
-  const [message, setMessage] = useState<Message>({
-    role: "pro",
-    message:
-      "안녕하십니까 형님 안녕하십니까 형님 안녕하십니까 형님 안녕하십니까 형님 안녕하십니까 형님",
-    name: "도끼 든 회색 곰",
-    createdAt: "11:02",
-  });
+  const myRole = "con";
+  const message: IMessage[] = [
+    {
+      messageId: 1,
+      role: "pro",
+      content: "안녕하십니까 형님",
+      name: "도끼 든 회색 곰",
+      createdAt: "11:02",
+    },
+    {
+      messageId: 2,
+      role: "con",
+      content:
+        "나는 낭만 고양이 나는 고양이가 되고 싶어요. 그렇게 나는 길고양이가 되었다고 한다..",
+      name: "총 든 토끼",
+      createdAt: "11:02",
+    },
+    {
+      messageId: 3,
+      role: "pro",
+      content: "아따맘마를 아세요?",
+      name: "노트북 하는 병아리",
+      createdAt: "11:02",
+    },
+  ];
 
   return (
     <div>
-      {message.role === myRole ? (
-        <MyMessage message={message} />
-      ) : (
-        <YourMessage message={message} />
-      )}
+      {message.map((message) => (
+        <div key={message.messageId}>
+          {message.role === myRole ? (
+            <MyMessage message={message} />
+          ) : (
+            <YourMessage message={message} />
+          )}
+        </div>
+      ))}
     </div>
   );
 }
