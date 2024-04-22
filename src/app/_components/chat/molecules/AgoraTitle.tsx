23fd5318a0cc23f1ba934c4f091faa6ff980c6df
H-sooyeon <wndqhr__@naver.com>
2024-04-22 20:10:58 +0900

    design: 아고라 주제 크기 키우기

--- src/app/_components/chat/molecules/AgoraTitle.tsx
+++ src/app/_components/chat/molecules/AgoraTitle.tsx
@@ -3,7 +3,7 @@ import SpeakerIcon from "@/assets/icons/SpeakerIcon";
 export default function AgoraTitle() {
   return (
     <div className="flex flex-col justify-center items-center w-full">
-      <p className="text-sm font-semibold flex justify-center items-center p-5">
+      <p className="text-base font-semibold flex justify-center items-center p-5">
         <SpeakerIcon className="w-18 mr-0.5rem" fill="#10AE5D" />
         국가 발전에는 유능한 독재자가 필요한 시기가 있다.
       </p>
