e86cbdd07c5ef628762a7c8ed8acb4b64945b9a5
H-sooyeon <wndqhr__@naver.com>
2024-04-22 20:10:26 +0900

    feat: message 객체에서 정보 추출

--- src/app/_components/chat/atoms/MyMessage.tsx
+++ src/app/_components/chat/atoms/MyMessage.tsx
@@ -1,21 +1,23 @@
 import { Message } from "@/app/model/Message";
 import Image from "next/image";
 
-export default function MyMessage({ message }: Message) {
+export default function MyMessage({ message }: { message: Message }) {
   return (
     <div className="flex justify-end items-start p-1rem h-full">
-      <div className="flex flex-col justify-end items-end h-full">
-        <div className="text-xxs mb-8">{message.createdAt}</div>
-      </div>
-      <div className="p-0.5rem flex flex-col justify-center items-end">
-        <div className="text-xs pb-5">{message.name}</div>
-        <div className="max-w-[50vw] whitespace-pre-line bg-red-200 rounded-tl-xl rounded-bl-xl rounded-br-xl p-0.5rem pl-14 pr-14 text-sm">
-          {message.message}
+      <div className="flex justify-end items-end">
+        <div className="flex flex-col justify-end items-end h-full">
+          <div className="text-xxs mb-8">{message.createdAt}</div>
+        </div>
+        <div className="p-0.5rem flex flex-col justify-center items-end">
+          <div className="text-xs pb-5">{message.name}</div>
+          <div className="max-w-[50vw] whitespace-pre-line bg-red-200 rounded-tl-xl rounded-bl-xl rounded-br-xl p-0.5rem pl-14 pr-14 text-sm">
+            {message.content}
+          </div>
         </div>
       </div>
       <div className="border-1 border-gray-300 w-fit rounded-xl ">
         <Image
-          src="/img/도끼 든 회색 곰.png"
+          src={`/img/${message.name}.png`}
           alt="사용자 프로필 이미지"
           width={60}
           height={60}
--- src/app/_components/chat/atoms/YourMessage.tsx
+++ src/app/_components/chat/atoms/YourMessage.tsx
@@ -1,12 +1,12 @@
 import { Message } from "@/app/model/Message";
 import Image from "next/image";
 
-export default function YourMessage({ message }: Message) {
+export default function YourMessage({ message }: { message: Message }) {
   return (
     <div className="flex justify-start items-start p-1rem h-full">
       <div className="border-1 border-gray-300 w-fit rounded-xl">
         <Image
-          src="/img/도끼 든 회색 곰.png"
+          src={`/img/${message.name}.png`}
           alt="사용자 프로필 이미지"
           width={60}
           height={60}
@@ -15,12 +15,14 @@ export default function YourMessage({ message }: Message) {
       </div>
       <div className="p-0.5rem flex flex-col justify-center items-start">
         <div className="text-xs pb-5">{message.name}</div>
-        <div className="max-w-[50vw] whitespace-pre-line bg-blue-200 rounded-tr-xl rounded-bl-xl rounded-br-xl p-0.5rem pl-14 pr-14 text-sm">
-          {message.message}
+        <div className="flex justify-start items-end">
+          <div className="max-w-[50vw] whitespace-pre-line bg-blue-200 rounded-tr-xl rounded-bl-xl rounded-br-xl p-0.5rem pl-14 pr-14 text-sm">
+            {message.content}
+          </div>
+          <div className="flex flex-col justify-end items-end h-full">
+            <div className="text-xxs pl-8 h-full">{message.createdAt}</div>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-col justify-end items-end h-full">
-        <div className="text-xxs mb-8">{message.createdAt}</div>
       </div>
     </div>
   );
