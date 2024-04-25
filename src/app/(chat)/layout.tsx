e3668098fe61698c59540b02459f2bcdb061d615
H-sooyeon <wndqhr__@naver.com>
2024-04-25 20:50:18 +0900

    feat: 아고라 참여 사용자 리스트 위치 page로 이동

--- src/app/(chat)/layout.tsx
+++ src/app/(chat)/layout.tsx
@@ -1,5 +1,3 @@
-import AgoraUserList from "../_components/chat/AgoraUserList";
-
 type Props = {
   children: React.ReactNode;
 };
@@ -8,7 +6,6 @@ export default function Layout({ children }: Props) {
   return (
     <div className="lg:flex justify-center items-center xl:w-[1580px] w-full">
       {children}
-      <AgoraUserList />
     </div>
   );
 }
