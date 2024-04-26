7c97a4f039f831627bf8b943869adc917128cabf
H-sooyeon <wndqhr__@naver.com>
2024-04-26 20:08:19 +0900

    design: under-tablet grid 4로 변경

--- src/app/_components/home/molecules/AgoraList.tsx
+++ src/app/_components/home/molecules/AgoraList.tsx
@@ -14,7 +14,7 @@ import Agora from "../atoms/Agora";
 
 export default function AgoraList() {
   return (
-    <div className="grid under-large:grid-cols-5 gap-x-1rem gap-y-1rem under-mobile:grid-cols-2 mobile:grid-cols-2 foldable:grid-cols-3 tablet:grid-cols-4 under-tablet:grid-cols-3 xl:grid-cols-6 sm:grid-cols-3 lg:grid-cols-5 under-xl:grid-cols-4">
+    <div className="grid under-large:grid-cols-5 gap-x-1rem gap-y-1rem under-mobile:grid-cols-2 mobile:grid-cols-2 foldable:grid-cols-3 tablet:grid-cols-4 under-tablet:grid-cols-4 xl:grid-cols-6 sm:grid-cols-3 lg:grid-cols-5 under-xl:grid-cols-4">
       <Agora />
       <Agora />
       <Agora />
