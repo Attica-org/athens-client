baad9a754a046d65c8010f93662dd26d003373c5
H-sooyeon <wndqhr__@naver.com>
2024-04-26 20:09:04 +0900

    design: tablet 크기일 때 화면 크기 조정 삭제

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function Layout({ children, modal }: Props) {
   return (
-    <div className="lg:flex justify-center items-center xl:w-[1580px] lg:w-[1024px] tablet:w-[706px] under-large:w-full ">
+    <div className="lg:flex justify-center items-center xl:w-[1580px] lg:w-[1024px] under-large:w-full ">
       <SideNav />
       {children}
       {modal}
