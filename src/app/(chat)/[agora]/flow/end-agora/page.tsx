ac37cf086762b030e9244b7091a46ef2b41a807f
H-sooyeon <wndqhr__@naver.com>
2024-04-26 22:10:06 +0900

    feat: 토론 종료 모달 창

---
+++ src/app/(chat)/[agora]/flow/end-agora/page.tsx
@@ -0,0 +1,13 @@
+import ModalBase from "@/app/_components/molecules/ModalBase";
+
+export default function Page() {
+  return (
+    <div>
+      <ModalBase title="토론 종료" removeIcon={false}>
+        <div>
+          <h1>hello</h1>
+        </div>
+      </ModalBase>
+    </div>
+  );
+}
