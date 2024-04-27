56957ccb9b704034f93f3f752712692383ab0ed8
H-sooyeon <wndqhr__@naver.com>
2024-04-28 00:00:42 +0900

    feat: 폭죽 이벤트 파일로 분리

---
+++ src/utils/Confetti.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import confetti from "canvas-confetti";
+import { useEffect, useRef } from "react";
+
+export default function ConfettiButton2() {
+  const duration = 35 * 1000;
+  const animationEnd = Date.now() + duration;
+  const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+  const intervalId = useRef<NodeJS.Timeout>();
+
+  function randomInRange(min: number, max: number) {
+    return Math.random() * (max - min) + min;
+  }
+
+  const handleConfetti = () => {
+    intervalId.current = setInterval(() => {
+      let timeLeft = animationEnd - Date.now();
+
+      var particleCount = 50 * (timeLeft / duration);
+      // since particles fall down, start a bit higher than random
+      confetti(
+        Object.assign({}, defaults, {
+          particleCount,
+          origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+        })
+      );
+      confetti(
+        Object.assign({}, defaults, {
+          particleCount,
+          origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+        })
+      );
+    }, 250);
+  };
+
+  useEffect(() => {
+    handleConfetti();
+
+    return () => {
+      clearInterval(intervalId.current);
+    };
+  });
+
+  return <div></div>;
+}
