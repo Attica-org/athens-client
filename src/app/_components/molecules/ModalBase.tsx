fb9522754029b8c47ca05d31047cacb321ec4d10
H-sooyeon <wndqhr__@naver.com>
2024-04-28 00:02:04 +0900

    feat: animation, removeIcon 존재 여부에 대한 애니메이션 적용

--- src/app/_components/molecules/ModalBase.tsx
+++ src/app/_components/molecules/ModalBase.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { MouseEventHandler, useEffect } from "react";
+import { MouseEventHandler, useEffect, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 import RemoveButton from "../atoms/RemoveButton";
 
@@ -8,17 +8,26 @@ type Props = {
   children: React.ReactNode;
   title: string;
   removeIcon: boolean;
+  animation: boolean;
 };
 
-export default function ModalBase({ children, title, removeIcon }: Props) {
+export default function ModalBase({
+  children,
+  title,
+  removeIcon,
+  animation,
+}: Props) {
   const router = useRouter();
+  const [opacity, setOpacity] = useState("opacity-0");
 
   useEffect(() => {
     document.body.style.overflow = "hidden";
+    setOpacity("opacity-100");
+
     return () => {
       document.body.style.overflow = "unset";
     };
-  }, []);
+  }, [animation]);
 
   const clickOutSideModal: MouseEventHandler<HTMLDivElement> = (e) => {
     if (e.target === e.currentTarget && removeIcon) router.back();
@@ -29,7 +38,11 @@ export default function ModalBase({ children, title, removeIcon }: Props) {
       onClick={clickOutSideModal}
       className="w-dvw h-dvh flex absolute justify-center items-center z-20 top-0 right-0 left-0 bottom-0 bg-opacity-20 bg-gray-600"
     >
-      <div className="under-mobile:mt-14rem bg-white bottom-7rem mobile:w-[70vw] pb-0.5rem under-mobile:pb-1rem under-mobile:w-[80vw] w-[50vw] lg:w-40rem relative rounded-2xl min-w-220">
+      <div
+        className={`${
+          animation && `transition duration-500 transform scale-100 `
+        } ${opacity} under-mobile:mt-14rem bg-white bottom-7rem mobile:w-[70vw] pb-0.5rem under-mobile:pb-1rem under-mobile:w-[80vw] w-[50vw] lg:w-40rem relative rounded-2xl min-w-220`}
+      >
         <h1 className="flex justify-center items-center mt-2rem text-md">
           {title}
         </h1>
