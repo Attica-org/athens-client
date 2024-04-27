9720c7d493b68efe182e64d2a102a93bd4cd0093
H-sooyeon <wndqhr__@naver.com>
2024-04-27 23:58:42 +0900

    feat: 메시지 입력 태그 form으로 변경

--- src/app/_components/chat/molecules/MessageInput.tsx
+++ src/app/_components/chat/molecules/MessageInput.tsx
@@ -5,13 +5,13 @@ import SendIcon from "@/assets/icons/SendIcon";
 export default function MessageInput() {
   return (
     <section className="flex border-t-1 sticky bottom-0 right-0 left-0 w-full bg-white">
-      <div className="pl-1.5rem p-12 flex flex-1 justify-start items-center">
+      <form className="pl-1.5rem p-12 flex flex-1 justify-start items-center">
         <input
           type="text"
           placeholder="Type a message"
           className="text-base w-full focus-visible:outline-none"
         />
-      </div>
+      </form>
       <div className="bg-athens-main p-12 cursor-pointer">
         <SendIcon className="w-1.5rem" fill="white" />
       </div>
