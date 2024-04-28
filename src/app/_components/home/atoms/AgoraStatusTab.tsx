3b4682f7f3f1d48e9523ec73e853605391d902e3
H-sooyeon <wndqhr__@naver.com>
2024-04-28 18:54:42 +0900

    feat: 선택한 아고라 상태에 따라 border line 주기

--- src/app/_components/home/atoms/AgoraStatusTab.tsx
+++ src/app/_components/home/atoms/AgoraStatusTab.tsx
@@ -1,12 +1,32 @@
+"use client";
+
+import { useState } from "react";
+
+type Status = "active" | "end";
+
 export default function AgoraStatusTab() {
-  // const state: "active" | "ended" = "active";
+  const [status, setStatus] = useState<Status>("active");
+
+  const changeStatus = () => {
+    setStatus(status === "active" ? "end" : "active");
+  };
 
   return (
     <nav className="flex flex-row justify-around items-center h-2rem w-full text-xs pl-5 pr-5">
-      <button className="border-b-1 border-athens-sub flex flex-1 justify-center p-6">
+      <button
+        onClick={changeStatus}
+        className={`border-b-1 ${
+          status === "active" && "border-athens-sub"
+        } flex flex-1 justify-center p-6`}
+      >
         활성화
       </button>
-      <button className="flex flex-1 justify-center p-6 border-b-1">
+      <button
+        onClick={changeStatus}
+        className={`flex flex-1 justify-center p-6 border-b-1 ${
+          status === "end" && "border-athens-sub"
+        }`}
+      >
         종료
       </button>
     </nav>
