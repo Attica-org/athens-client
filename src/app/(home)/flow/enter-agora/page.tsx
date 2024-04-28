c789412bb27a31e5913693d252517b02dd30ad41
H-sooyeon <wndqhr__@naver.com>
2024-04-28 20:42:31 +0900

    feat: 모달 컴포넌트에서 Main 컴포넌트 제거

--- src/app/(home)/flow/enter-agora/page.tsx
+++ src/app/(home)/flow/enter-agora/page.tsx
@@ -1,10 +1,8 @@
 import EnterAgora from "@/app/_components/home/templates/EnterAgora";
-import Main from "@/app/_components/home/templates/Main";
 
 export default function Page() {
   return (
     <>
-      <Main />
       <EnterAgora />
     </>
   );
