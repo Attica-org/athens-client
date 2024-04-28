7423c32097aa231baac33fee1fc6f888df758578
H-sooyeon <wndqhr__@naver.com>
2024-04-28 18:55:00 +0900

    feat: category에 id 추가

--- src/app/_components/home/molecules/CategoryButtonList.tsx
+++ src/app/_components/home/molecules/CategoryButtonList.tsx
@@ -1,22 +1,23 @@
 "use client";
 
 import CategoryButton from "../atoms/CategoryButton";
+import { useEffect, useState } from "react";
+import { Category } from "@/app/model/Category";
+
 import Swiper from "swiper";
 import "swiper/css";
 import "swiper/css/free-mode";
 import "swiper/css/mousewheel";
-import { useEffect } from "react";
-
-const categoryList = [
-  { innerText: "전체", isActive: true },
-  { innerText: "지식/공부", isActive: false },
-  { innerText: "문화/예술", isActive: false },
-  { innerText: "음식/여행", isActive: false },
-  { innerText: "일상/취미", isActive: false },
-];
 
 export default function CategoryButtonList() {
   const search = "검색";
+  const [categoryList, setCategoryList] = useState<Category[]>([
+    { id: 1, innerText: "전체", isActive: true },
+    { id: 2, innerText: "지식/공부", isActive: false },
+    { id: 3, innerText: "문화/예술", isActive: false },
+    { id: 4, innerText: "음식/여행", isActive: false },
+    { id: 5, innerText: "일상/취미", isActive: false },
+  ]);
 
   useEffect(() => {
     const swiper = new Swiper(".swiper", {
@@ -39,13 +40,26 @@ export default function CategoryButtonList() {
     };
   }, []);
 
+  const selectCategory = (id: number) => {
+    setCategoryList(
+      categoryList.map((category) => ({
+        ...category,
+        isActive: category.id === id,
+      }))
+    );
+  };
+
   if (!search) return null;
   return (
-    <div className="w-full mt-10 mb-0 pb-0 pr-1rem pl-0.5rem flex text-nowrap overflow-hidden ml-5">
+    <div className="w-full mt-10 mb-0 pb-0 pl-0.5rem pr-0.5rem flex text-nowrap overflow-hidden ml-5">
       <div className="swiper pr-1rem">
         <div className="swiper-wrapper pr-1rem">
           {categoryList.map((category) => (
-            <div className="swiper-slide" key={category.innerText}>
+            <div
+              onClick={() => selectCategory(category.id)}
+              className="swiper-slide"
+              key={category.innerText}
+            >
               <CategoryButton
                 innerText={category.innerText}
                 isActive={category.isActive}
