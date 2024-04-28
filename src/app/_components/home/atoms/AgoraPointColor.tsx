32e570e4b8788567a95945498e2c8087d3cc38dc
H-sooyeon <wndqhr__@naver.com>
2024-04-28 21:43:46 +0900

    feat: svg 크기 px로 수정

--- src/app/_components/home/atoms/AgoraPointColor.tsx
+++ src/app/_components/home/atoms/AgoraPointColor.tsx
@@ -7,15 +7,16 @@ type Props = {
 
 export default function AgoraPointColor({ id, isCheck }: Props) {
   const color = COLOR[id - 1];
+
   return (
     <>
       <div
-        className={`cursor-pointer w-2rem h-2rem under-mobile:w-1.5rem under-mobile:h-1.5rem rounded-full bg-agora-point-color1 ${color} mr-5`}
+        className={`${color} cursor-pointer w-2rem h-2rem under-mobile:w-1.5rem under-mobile:h-1.5rem rounded-full mr-5`}
       >
         {isCheck && (
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            height="2rem under-mobile:w-1.5rem"
+            height="25"
             viewBox="0 -960 960 960"
             className="flex justify-center items-center w-full h-full text-white"
           >
