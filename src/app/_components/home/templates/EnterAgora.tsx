cae8ef32487336a35eb679c9ac8f41ce5120abc8
H-sooyeon <wndqhr__@naver.com>
2024-04-28 17:30:06 +0900

    feat: animation 효과 적용 유무 추가

--- src/app/_components/home/templates/EnterAgora.tsx
+++ src/app/_components/home/templates/EnterAgora.tsx
@@ -40,7 +40,7 @@ export default function EnterAgora() {
   };
 
   return (
-    <ModalBase title="아고라 입장" removeIcon={true}>
+    <ModalBase title="아고라 입장" removeIcon={true} animation={false}>
       <p className="text-base p-1rem pt-1.5rem pb-1.5rem flex justify-center items-cener text-center break-keep font-medium">
         {'"'}국가 발전에 유능한 독재자가 필요한 시기가 있다.{'"'}
       </p>
@@ -87,7 +87,7 @@ export default function EnterAgora() {
             selectedPosition === "pro"
               ? "text-white bg-blue-400"
               : "text-blue-400 bg-white"
-          } rounded-xl mr-7 text-xs tablet:text-sm `}
+          } rounded-xl mr-4 text-xs tablet:text-sm `}
         >
           찬성
         </button>
@@ -97,7 +97,7 @@ export default function EnterAgora() {
             selectedPosition === "con"
               ? "text-white bg-red-400"
               : "text-red-400 bg-white"
-          } rounded-xl mr-7 text-xs tablet:text-sm`}
+          } rounded-xl mr-4 text-xs tablet:text-sm`}
         >
           반대
         </button>
