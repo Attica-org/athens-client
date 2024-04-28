693dcd76350524a14fc7e5b53eb419d1b8e5a47a
H-sooyeon <wndqhr__@naver.com>
2024-04-28 17:27:22 +0900

    design: margin 값 없애기

--- src/app/_components/home/atoms/CategoryButton.tsx
+++ src/app/_components/home/atoms/CategoryButton.tsx
@@ -8,7 +8,7 @@ export default function CategoryButton({ innerText, isActive }: Props) {
     <button
       className={`${
         isActive ? "bg-athens-sub" : "bg-athens-gray"
-      } flex justify-center p-10 pl-1.5rem pr-1.5rem rounded-full text-xs ml-5`}
+      } flex justify-center p-10 pl-1.5rem pr-1.5rem rounded-full text-xs`}
     >
       {innerText}
     </button>
