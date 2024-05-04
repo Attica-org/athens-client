46e4cb2f209880d2415b4a171be712863e5794cd
H-sooyeon <wndqhr__@naver.com>
2024-05-04 23:59:41 +0900

    [#2] refactor: 버튼으로 수정 가능한 number input atom 컴포넌트

---
+++ src/app/(home)/_components/atoms/ControlNumberInput.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+type Props = {
+  label: string;
+  value: number;
+  handleChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleButtonClick: (action: 'DECLEASE' | 'INCREASE') => void;
+  increaseLabel: string;
+  decreaseLabel: string;
+  inputLabel: string;
+};
+
+export default function ModifyNumberInput({
+  label,
+  value,
+  handleChange,
+  handleButtonClick,
+  increaseLabel,
+  decreaseLabel,
+  inputLabel,
+}: Props) {
+  return (
+    <label htmlFor="modify-number-input" className="text-sm under-mobile:text-xs flex justify-center items-center">
+      {label}
+      <div className="ml-0.5rem pl-7 pr-7 border-1 border-athens-gray rounded-md dark:border-gray-500 flex justify-center items-center">
+        <button
+          type="button"
+          aria-label={decreaseLabel}
+          className="text-xl cursor-pointer"
+          onClick={() => handleButtonClick('DECLEASE')}
+        >
+          -
+        </button>
+        <input
+          id="modify-number-input"
+          aria-label={inputLabel}
+          value={value}
+          onChange={handleChange}
+          type="number"
+          className="text-center max-w-32 w-32 text-xs input-number-hide focus-visible:outline-none dark:bg-dark-bg-light"
+        />
+        <button
+          type="button"
+          aria-label={increaseLabel}
+          className="text-xl cursor-pointer"
+          onClick={() => handleButtonClick('INCREASE')}
+        >
+          +
+        </button>
+      </div>
+    </label>
+  );
+}
