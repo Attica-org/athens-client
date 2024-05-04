54239660140cc1d489b97f91bd5b9c159a01fc63
H-sooyeon <wndqhr__@naver.com>
2024-05-04 23:09:31 +0900

    [#2] design: 라이트모드 검색 창 border 적용

--- src/app/(home)/create-agora/page.tsx
+++ src/app/(home)/create-agora/page.tsx
@@ -70,7 +70,7 @@ export default function CreateAgora() {
             aria-label="생성할 아고라 주제 입력창"
             type="text"
             placeholder="토론 할 주제를 입력해주세요."
-            className="placeholder:text-athens-gray-thick w-full p-0.5rem text-sm border-0 border-athens-gray rounded-md dark:bg-dark-light-300 dark:placeholder:text-white dark:placeholder:text-opacity-85 dark:text-white"
+            className="placeholder:text-athens-gray-thick w-full p-0.5rem text-sm border-1 border-gray-300 rounded-md dark:bg-dark-light-300 dark:placeholder:text-white dark:placeholder:text-opacity-85 dark:border-0 dark:text-white"
           />
           <div className="flex justify-around flex-col w-full">
             <section className="mt-1.5rem w-full">
