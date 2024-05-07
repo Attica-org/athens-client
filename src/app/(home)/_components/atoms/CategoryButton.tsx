99e0ea2c1438730000a3b7a4cc42490161ece090
H-sooyeon <wndqhr__@naver.com>
2024-05-07 20:02:03 +0900

    [#2] design: 카테고리 버튼 border 항상 유지

--- src/app/(home)/_components/atoms/CategoryButton.tsx
+++ src/app/(home)/_components/atoms/CategoryButton.tsx
@@ -9,7 +9,7 @@ export default function CategoryButton({ innerText, isActive }: Props) {
   return (
     <div
       className={`${
-        isActive ? 'bg-athens-sub' : 'bg-athens-gray dark:bg-dark-bg-light dark:text-white dark:border-1 dark:border-gray-500'
+        isActive ? 'bg-athens-sub text-black border-1 border-athens-sub' : 'bg-athens-gray dark:bg-dark-bg-light dark:text-white dark:border-1 dark:border-gray-500'
       } flex justify-center p-8 pl-1.5rem pr-1.5rem rounded-full text-xs`}
     >
       {innerText}
