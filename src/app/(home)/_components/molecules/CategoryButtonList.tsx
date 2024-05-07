9960a364795790bf466dd7803c26eb99835470a3
H-sooyeon <wndqhr__@naver.com>
2024-05-07 21:08:06 +0900

    [#5] feat: 카테고리 선택 시 url 변경

--- src/app/(home)/_components/molecules/CategoryButtonList.tsx
+++ src/app/(home)/_components/molecules/CategoryButtonList.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
-import { Category } from '@/app/model/Category';
+import React, { useCallback, useEffect, useState } from 'react';
 import AGORACATEGORY from '@/constants/agoraCategory';
 import Swiper from 'swiper';
+import { useRouter, useSearchParams } from 'next/navigation';
 import CategoryButton from '../atoms/CategoryButton';
 
 import 'swiper/css';
@@ -12,7 +12,16 @@ import 'swiper/css/mousewheel';
 
 export default function CategoryButtonList() {
   const search = '검색';
-  const [categoryList, setCategoryList] = useState<Category[]>(AGORACATEGORY);
+  const [selectedCategoryId, setSelectedCategoryId] = useState<number>(0);
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  const changeParams = useCallback(() => {
+    const newSearchParams = new URLSearchParams(searchParams);
+
+    newSearchParams.set('category', AGORACATEGORY[selectedCategoryId].innerText);
+    router.replace(`/?${newSearchParams.toString()}`);
+  }, [router, searchParams, selectedCategoryId]);
 
   useEffect(() => {
     const swiper = new Swiper('.swiper', {
@@ -35,13 +44,14 @@ export default function CategoryButtonList() {
     };
   }, []);
 
+  useEffect(() => {
+    changeParams();
+  }, [changeParams]);
+
   const selectCategory = (id: number) => {
-    setCategoryList(
-      categoryList.map((category) => ({
-        ...category,
-        isActive: category.id === id,
-      })),
-    );
+    changeParams();
+
+    setSelectedCategoryId(id);
   };
 
   if (!search) return null;
@@ -49,7 +59,7 @@ export default function CategoryButtonList() {
     <div className="w-full mt-10 mb-0 pb-0 pl-0.5rem pr-0.5rem flex text-nowrap overflow-hidden ml-5">
       <div className="swiper pr-1rem w-full">
         <div className="swiper-wrapper pr-1rem">
-          {categoryList.map((category) => (
+          {AGORACATEGORY.map((category) => (
             <button
               type="button"
               aria-label="카테고리 선택"
@@ -59,7 +69,7 @@ export default function CategoryButtonList() {
             >
               <CategoryButton
                 innerText={category.innerText}
-                isActive={category.isActive}
+                isActive={category.id === selectedCategoryId}
               />
             </button>
           ))}
--- src/constants/agoraCategory.ts
+++ src/constants/agoraCategory.ts
@@ -1,9 +1,9 @@
 const AGORACATEGORY = [
-  { id: 1, innerText: '전체', isActive: true },
-  { id: 2, innerText: '지식/공부', isActive: false },
-  { id: 3, innerText: '문화/예술', isActive: false },
-  { id: 4, innerText: '음식/여행', isActive: false },
-  { id: 5, innerText: '일상/취미', isActive: false },
+  { id: 0, innerText: '전체' },
+  { id: 1, innerText: '지식/공부' },
+  { id: 2, innerText: '문화/예술' },
+  { id: 3, innerText: '음식/여행' },
+  { id: 4, innerText: '일상/취미' },
 ];
 
 export default AGORACATEGORY;
