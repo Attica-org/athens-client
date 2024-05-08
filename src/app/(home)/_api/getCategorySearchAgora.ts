5b2d6466fe21049601ca15dfde784cfdc2dc90a9
H-sooyeon <wndqhr__@naver.com>
2024-05-08 21:27:53 +0900

    [#5] feat: 아고라 카테고리 검색 api 호출 작성

---
+++ src/app/(home)/_api/getCategorySearchAgora.ts
@@ -0,0 +1,32 @@
+import { Agora } from '@/app/model/Agora';
+import { QueryFunction } from '@tanstack/react-query';
+
+type SearchParams = {
+  status: string,
+  category: string,
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export const getCategorySearchAgora:QueryFunction<
+Agora[],
+[_1: string, _2: string, _3: string, searchParams: SearchParams],
+Partial<number>> = async ({ queryKey, pageParam = 0 }) => {
+  const searchParams = queryKey[3];
+
+  const urlSearchParams = new URLSearchParams(searchParams);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/agoras?${urlSearchParams.toString()}&cursor=${pageParam}`, {
+    next: {
+      tags: ['agoras', 'search', 'category', searchParams.category, searchParams.status],
+    },
+    credentials: 'include',
+    cache: 'no-store',
+  });
+
+  if (!res.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  const data = res.json();
+
+  return data;
+};
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,15 +1,63 @@
 import { HttpResponse, http } from 'msw';
 
-const User = [
-  { id: 'elonmusk', nickname: 'Elon Musk', image: '/yRsRRjGO.jpg' },
-  { id: 'zeroch0', nickname: '제로초', image: '/5Udwvqim.jpg' },
-];
+const delay = (ms: number) => new Promise((resolve) => {
+  setTimeout(resolve, ms);
+});
 
 // eslint-disable-next-line import/prefer-default-export
 export const handlers = [
-  http.post('/api/login', () => HttpResponse.json(User[0], {
-    headers: {
-      'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/',
-    },
-  })),
+  http.get('/api/v1/agoras', async ({ request }) => {
+    delay(500);
+    const url = new URL(request.url);
+    // const status = url.searchParams.get('status');
+    // const category = url.searchParams.get('category');
+    const cursor = parseInt(url.searchParams.get('cursor') as string, 10) || 0;
+
+    return HttpResponse.json([
+      {
+        id: cursor + 1,
+        agoraTitle: '소득 불평등 심화가 사회 양극화를 야기하는가?',
+        agoraColor: 'bg-yellow-400',
+        participants: {
+          pros: 10,
+          cons: 10,
+          observer: 10,
+        },
+        status: 'queued',
+      },
+      {
+        id: cursor + 2,
+        agoraTitle: '인공지능 기술의 발전이 사회의 일자리를 빼앗는가?',
+        agoraColor: 'bg-yellow-400',
+        participants: {
+          pros: 10,
+          cons: 10,
+          observer: 10,
+        },
+        status: 'running',
+      },
+      {
+        id: cursor + 3,
+        agoraTitle: '소득 불평등 심화가 사회 양극화를 야기하는가?',
+        agoraColor: 'bg-yellow-400',
+        participants: {
+          pros: 10,
+          cons: 10,
+          observer: 10,
+        },
+        status: 'queued',
+      },
+      {
+        id: cursor + 4,
+        agoraTitle: '인공지능 기술의 발전이 사회의 일자리를 빼앗는가?',
+        agoraColor: 'bg-yellow-400',
+        participants: {
+          pros: 10,
+          cons: 10,
+          observer: 10,
+        },
+        status: 'running',
+      },
+    ]);
+  }),
 ];
