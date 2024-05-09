8374b7167a2a675c60c969e460c3779c98e67709
H-sooyeon <wndqhr__@naver.com>
2024-05-09 20:35:04 +0900

    [#5] feat: 아고라 검색창 전역 상태로 관리

--- src/app/(home)/_components/atoms/SearchBar.tsx
+++ src/app/(home)/_components/atoms/SearchBar.tsx
@@ -2,30 +2,41 @@
 
 import RemoveIcon from '@/assets/icons/RemoveIcon';
 import SearchIcon from '@/assets/icons/SearchIcon';
-import React, { ChangeEventHandler, useState } from 'react';
+import { useSearchStore } from '@/store/search';
+import { useRouter } from 'next/navigation';
+import React, { ChangeEventHandler } from 'react';
 
 export default function SearchBar() {
-  const [inputText, setInputText] = useState<string>('');
+  const { search, setSearch, reset } = useSearchStore();
+  const router = useRouter();
 
   const removeInputText = () => {
-    setInputText('');
+    reset();
   };
 
   const changeInputText: ChangeEventHandler<HTMLInputElement> = (e) => {
-    setInputText(e.target.value);
+    setSearch(e.target.value);
+  };
+
+  const handleSearch = () => {
+    router.push(`/search?q=${search}&st=active`);
   };
 
   return (
     <div className="bg-athens-gray rounded-md p-4 flex justify-center items-center dark:bg-dark-light-300">
-      <SearchIcon className="w-22 ml-10" />
-      <input
-        aria-label="아고라 검색창"
-        type="text"
-        className="w-full text-sm bg-athens-gray border-0 focus:outline-none pl-1rem placeholder:font-normal placeholder:text-athens-gray-thick dark:bg-dark-light-300 dark:placeholder:text-white dark:placeholder:text-opacity-85 dark:text-white"
-        placeholder="검색"
-        value={inputText}
-        onChange={changeInputText}
-      />
+      <button type="button" aria-label="검색하기" onClick={handleSearch}>
+        <SearchIcon className="w-22 ml-10" />
+      </button>
+      <form action={handleSearch} className="w-full">
+        <input
+          aria-label="아고라 검색창"
+          type="text"
+          className="w-full text-sm bg-athens-gray border-0 focus:outline-none pl-1rem placeholder:font-normal placeholder:text-athens-gray-thick dark:bg-dark-light-300 dark:placeholder:text-white dark:placeholder:text-opacity-85 dark:text-white"
+          placeholder="검색"
+          value={search}
+          onChange={changeInputText}
+        />
+      </form>
       <div className="flex justify-center items-center w-1.5rem h-1.5rem">
         <RemoveIcon
           className="w-20 cursor-pointer"
