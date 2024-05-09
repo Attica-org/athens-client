fc4cfddeeb4bf831374d7150e5617551a80c8ed0
H-sooyeon <wndqhr__@naver.com>
2024-05-09 20:33:06 +0900

    [#5] feat: 아고라 키워드 검색 api 요청 함수

---
+++ src/app/(home)/_api/getAgoraKeywordSearch.tsx
@@ -0,0 +1,33 @@
+import { Agora } from '@/app/model/Agora';
+import { QueryFunction } from '@tanstack/react-query';
+
+type SearchParams = {
+  st: string,
+  cat: string,
+  q?: string,
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export const getAgoraKeywordSearch:QueryFunction<
+Agora[],
+[_1: string, _2: string, _3: string, searchParams: SearchParams],
+Partial<number>> = async ({ queryKey, pageParam = 0 }) => {
+  const searchParams = queryKey[3];
+
+  const urlSearchParams = new URLSearchParams(searchParams);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/agoras?${urlSearchParams.toString()}&cursor=${pageParam}`, {
+    next: {
+      tags: ['agoras', 'search', 'keyword', searchParams.q as string, searchParams.st],
+    },
+    credentials: 'include',
+    cache: 'no-store',
+  });
+
+  if (!res.ok) {
+    throw new Error('Network response was not ok');
+  }
+
+  const result = res.json().then((data) => data.response.agoras);
+
+  return result;
+};
