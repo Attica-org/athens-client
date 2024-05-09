9b729dc13c91e56bac72c14224bcc6c875156d25
H-sooyeon <wndqhr__@naver.com>
2024-05-09 20:41:01 +0900

    [#5] feat: 아고라 키워드 검색하기 mock api 연결

--- src/app/(home)/_components/molecules/SearchAgora.tsx
+++ src/app/(home)/_components/molecules/SearchAgora.tsx
@@ -2,10 +2,18 @@
 
 import React, { KeyboardEventHandler } from 'react';
 import { useRouter } from 'next/navigation';
+import { Agora } from '@/app/model/Agora';
 import UserImage from '../../../_components/atoms/UserImage';
 
-export default function SearchAgora() {
+type Props = {
+  agora: Agora
+};
+
+export default function SearchAgora({ agora }: Props) {
   const router = useRouter();
+  const {
+    id, agoraTitle, agoraColor, participants, createdAt, status,
+  } = agora;
 
   // TODO: 아고라 id를 받아서 해당 아고라로 이동
   const enterAgora = () => {
@@ -19,38 +27,46 @@ export default function SearchAgora() {
   };
 
   return (
-    <article className="w-full">
+    <article id={`${id}`} className="w-full">
       <div role="button" tabIndex={0} aria-label="아고라" onKeyDown={handleKeyDownEnterAgora} onClick={enterAgora} className="w-full flex mb-15 pb-15 pl-1rem pr-1rem justify-center items-center cursor-pointer border-b-1 border-gray-100 dark:border-0">
         <div className="flex-1 p-0.5rem pl-0">
           <div className="flex justify-between items-start mb-12">
             <h3 className="text-base under-mobile:text-xs break-words break-keep line-clamp-2 max-w-prose dark:text-white dark:text-opacity-85">
-              국가 발전에는 유능한 독재자가 필요한 시기가 있다
+              {agoraTitle}
             </h3>
           </div>
-          <div className="text-xs text-nowrap">
-            <span className="text-blue-500 dark:text-dark-pro-color">
-              찬성
-              <span className="text-athens-gray-thick pl-3 dark:text-dark-line">
-                10명
-                <span aria-hidden> | </span>
+          <div className="flex justify-between items-center">
+            <div className="text-xs text-nowrap">
+              <span className="text-blue-500 dark:text-dark-pro-color">
+                찬성
+                <span className="text-athens-gray-thick pl-3 dark:text-dark-line">
+                  {participants.pros}
+                  명
+                  <span aria-hidden> | </span>
+                </span>
               </span>
-            </span>
-            <span className="text-red-500 dark:text-dark-con-color">
-              반대
-              <span className="text-athens-gray-thick pl-3 dark:text-dark-line">
-                10명
-                <span aria-hidden> | </span>
+              <span className="text-red-500 dark:text-dark-con-color">
+                반대
+                <span className="text-athens-gray-thick pl-3 dark:text-dark-line">
+                  {participants.cons}
+                  명
+                  <span aria-hidden> | </span>
+                </span>
               </span>
-            </span>
-            <span className="dark:text-white dark:text-opacity-80">
-              관찰자
-              <span className="pl-3 text-athens-gray-thick dark:text-dark-line">10명</span>
-            </span>
+              <span className="dark:text-white dark:text-opacity-80">
+                관찰자
+                <span className="pl-3 text-athens-gray-thick dark:text-dark-line">
+                  {participants.observer}
+                  명
+                </span>
+              </span>
+            </div>
+            <div className="text-xs">{createdAt}</div>
           </div>
         </div>
         <div className="relative">
-          <UserImage className="w-67 h-67 bg-yellow-400 rounded-3xl" w={67} h={67} />
-          <span className="absolute top-0 left-53 inline-block w-13 h-13 bg-red-400 rounded-full ml-3" />
+          <UserImage className={`w-67 h-67 bg-${agoraColor} rounded-3xl`} w={67} h={67} />
+          <span className={`absolute top-0 left-53 inline-block w-13 h-13 ${status === 'queued' ? 'bg-athens-button' : 'bg-red-400'} rounded-full ml-3`} />
         </div>
       </div>
     </article>
--- src/app/(home)/_components/organisms/SearchAgoraList.tsx
+++ src/app/(home)/_components/organisms/SearchAgoraList.tsx
@@ -1,21 +1,50 @@
-import React from 'react';
-import SearchAgora from '../molecules/SearchAgora';
+'use client';
 
-export default function SearchAgoraList() {
+import React, { useEffect } from 'react';
+import { InfiniteData, useInfiniteQuery } from '@tanstack/react-query';
+import { Agora as IAgora } from '@/app/model/Agora';
+import { useInView } from 'react-intersection-observer';
+import SearchAgora from '../molecules/SearchAgora';
+import { getAgoraKeywordSearch } from '../../_api/getAgoraKeywordSearch';
+
+type Props = {
+  searchParams: { st: string, cat: string, q?: string }
+};
+
+export default function SearchAgoraList({ searchParams }: Props) {
+  const {
+    data, hasNextPage, fetchNextPage, isFetching,
+  } = useInfiniteQuery<IAgora[], Object, InfiniteData<IAgora[]>, [_1: string, _2: string, _3:string, Props['searchParams']], number>({
+    queryKey: ['agoras', 'search', 'keyword', searchParams],
+    queryFn: getAgoraKeywordSearch,
+    staleTime: 60 * 1000,
+    gcTime: 500 * 1000,
+    initialPageParam: 0,
+    getNextPageParam: (lastPage) => lastPage.at(-1)?.id,
+  });
+
+  const { ref, inView } = useInView({
+    threshold: 0,
+    delay: 0,
+    rootMargin: '-80px',
+  });
+
+  useEffect(() => {
+    if (inView && hasNextPage && !isFetching) {
+      fetchNextPage();
+    }
+  }, [inView, hasNextPage, fetchNextPage, isFetching]);
+
   return (
     <>
-      <SearchAgora />
-      <SearchAgora />
-      <SearchAgora />
-      <SearchAgora />
-      <SearchAgora />
-      <SearchAgora />
-      <SearchAgora />
-      <SearchAgora />
-      <SearchAgora />
-      <SearchAgora />
-      <SearchAgora />
-      <SearchAgora />
+      {data?.pages.map((page) => (
+        <React.Fragment key={page[0].id}>
+          {page.map((agora) => (
+            <SearchAgora key={agora.id} agora={agora} />
+          ))}
+        </React.Fragment>
+      ))}
+      <div ref={ref} className="h-50 bg-red-400 w-full" />
     </>
   );
 }
