cf523f3e9e66e327dec4b8bc3305c471e8964b5b
H-sooyeon <wndqhr__@naver.com>
2024-05-12 21:59:43 +0900

    [#5] feat: 아고라 카테고리 value 값 추가

--- src/constants/agoraCategory.ts
+++ src/constants/agoraCategory.ts
@@ -1,9 +1,9 @@
 const AGORACATEGORY = [
-  { id: 0, innerText: '전체' },
-  { id: 1, innerText: '지식/공부' },
-  { id: 2, innerText: '문화/예술' },
-  { id: 3, innerText: '음식/여행' },
-  { id: 4, innerText: '일상/취미' },
+  { id: 0, innerText: '전체', value: 'all' },
+  { id: 1, innerText: '지식/공부', value: 'study' },
+  { id: 2, innerText: '문화/예술', value: 'culture' },
+  { id: 3, innerText: '음식/여행', value: 'food' },
+  { id: 4, innerText: '일상/취미', value: 'daily' },
 ];
 
 export default AGORACATEGORY;
