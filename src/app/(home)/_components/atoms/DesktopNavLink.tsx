f2051d306c526d11d36d6f0e31c8078c32762478
H-sooyeon <wndqhr__@naver.com>
2024-05-13 17:29:44 +0900

    [#8] refactor: server component로 변경

--- src/app/(home)/_components/atoms/DesktopNavLink.tsx
+++ src/app/(home)/_components/atoms/DesktopNavLink.tsx
@@ -1,7 +1,4 @@
-'use client';
-
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 import React from 'react';
 
 type Props = {
@@ -19,17 +16,13 @@ export default function DesktopNavLink({
   innerText,
   label = '경로 이동',
 }: Props) {
-  const segment = usePathname();
-
   return (
     <li>
       <Link aria-label={label} href={href} className={className}>
         <div className="hover:bg-gray-100 dark:hover:bg-dark-light-300 h-3rem p-1rem inline-flex flex-row justify-center items-center mb-1rem rounded-full">
           {children}
           <span
-            className={`pl-1rem text-sm dark:text-dark-line-semilight ${
-              segment === `${href}` ? 'font-bold' : ''
-            }`}
+            className="pl-1rem text-sm dark:text-dark-line-semilight"
           >
             {innerText}
           </span>
