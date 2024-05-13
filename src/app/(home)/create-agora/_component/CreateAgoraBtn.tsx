bf69de4c2381d1bc60056d0d5401597672ef0f1e
H-sooyeon <wndqhr__@naver.com>
2024-05-13 19:36:11 +0900

    [#9] feat: 생성 버튼 클릭 시 아고라 입장 모달 띄우기

--- src/app/(home)/create-agora/_component/CreateAgoraBtn.tsx
+++ src/app/(home)/create-agora/_component/CreateAgoraBtn.tsx
@@ -1,53 +1,69 @@
 'use client';
 
 import { useMutation } from '@tanstack/react-query';
-import React from 'react';
+import React, { useState } from 'react';
 import { useCreateAgora } from '@/store/create';
-import { useShallow } from 'zustand/react/shallow';
-// import { useRouter } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import { useAgora } from '@/store/agora';
 import { postCreateAgora } from '../../_api/postCreateAgora';
 
+type Agora = {
+  title: string,
+  category: string,
+  color: string,
+  capacity: number,
+  duration: number | null,
+};
+
 function CreateAgoraBtn() {
-  const {
-    title, category, capacity, duration, color,
-  } = useCreateAgora(useShallow((state) => ({
-    title: state.title,
-    category: state.category,
-    capacity: state.capacity,
-    duration: state.duration,
-    color: state.color,
-  })));
-  // const router = useRouter();
+  const [createAgora, setCreateAgora] = useState<Agora>({
+    title: '',
+    category: 'all',
+    color: 'agora-point-color1',
+    capacity: 5,
+    duration: 60,
+  });
+  const router = useRouter();
 
   const mutation = useMutation({
     mutationFn: async () => {
       const info = {
-        title,
-        category,
-        color,
-        capacity,
-        duration,
+        ...createAgora,
       };
       return postCreateAgora(info);
     },
     onSuccess: async (response) => {
-      console.log(response);
-      // router.push('/agora/flow/enter-agora');
+      const { setSelectedAgora } = useAgora.getState();
+      setSelectedAgora({ id: response.id, title: createAgora.title });
+
+      const { reset } = useCreateAgora.getState();
+      reset();
+
+      router.push('/flow/enter-agora');
     },
     onError: (error) => {
-      console.dir(error);
+      console.error(error);
       alert('문제가 발생했습니다. 다시 시도해주세요.');
     },
   });
 
-  const createAgora = () => {
+  const handleClick = () => {
+    const {
+      title, category, color, capacity, duration,
+    } = useCreateAgora.getState();
+    setCreateAgora({
+      title, category, color, capacity, duration,
+    });
+
+    console.log('title', title);
+    if (title.trim() === '') return;
     mutation.mutate();
   };
 
   return (
     <div className="mt-1rem w-full">
       <button
-        onClick={createAgora}
+        onClick={handleClick}
         type="button"
         aria-label="아고라 생성하기"
         className="w-full bg-athens-main text-white font-semibold pt-10 pb-10 under-mobile:pt-10 under-mobile:pb-10 under-mobile:mt-1rem text-base rounded-lg"
