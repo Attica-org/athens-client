3845741bafa67bce801a1a7c3981b49fbd54a5bb
H-sooyeon <wndqhr__@naver.com>
2024-05-13 20:37:46 +0900

    [#10] feat: 로더 크기 props로 전달받기

--- src/app/(home)/loading.tsx
+++ src/app/(home)/loading.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
-export default function Loading() {
+type Props = {
+  w?: string;
+  h?: string;
+};
+
+export default function Loading({ w = '35', h = '35' }: Props) {
+  const size = `w-${w} h-${h}`;
+
   return (
-    <div role="status" className="m-5">
+    <div role="status" className="m-5 flex justify-center items-center">
       <svg
         aria-hidden="true"
-        className="w-35 h-35 text-gray-200 animate-spin dark:text-gray-600 fill-athens-main"
+        className={`${size} text-gray-200 animate-spin dark:text-gray-600 fill-athens-main`}
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
