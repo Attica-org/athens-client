6c84aa1244207761696abbc6c4fa081ed173b0bf
H-sooyeon <wndqhr__@naver.com>
2024-05-15 19:49:22 +0900

    [#10] design: overflow-x hidden 적용

--- src/app/(main)/home/layout.tsx
+++ src/app/(main)/home/layout.tsx
@@ -14,7 +14,7 @@ export default function Layout({ children }: Props) {
   return (
     <>
       <SideNav />
-      <section className="overflow-hidden scrollbar-hide flex flex-col flex-1 h-dvh max-lg:pb-3rem min-w-270 flex-grow max-width-screen dark:bg-dark-bg-light">
+      <section className="overflow-x-hidden scrollbar-hide flex flex-col flex-1 h-dvh max-lg:pb-3rem min-w-270 flex-grow max-width-screen dark:bg-dark-bg-light">
         <header className="p-1rem pb-0 relative min-w-270">
           <PageTitle
             title="아고라 검색"
