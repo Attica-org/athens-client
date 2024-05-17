5bb41a2032578d9c2466622e1012c3dd9a0badf3
H-sooyeon <wndqhr__@naver.com>
2024-05-17 17:51:59 +0900

    [#8] refactor: page 파일 함수 이름 Page로 변경

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Main from '../_components/templates/Main';
 
 type Props = {
-  searchParams: { status: string, category: string, q?: string }
+  searchParams: { status?: string, category?: string, q?: string }
 };
 
-export default function Home({ searchParams }: Props) {
+export default function Page({ searchParams }: Props) {
   return (
     <Main searchParams={searchParams} />
   );
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 
-export default async function Home() {
+export default async function Page() {
   redirect('/home');
   return null;
 }
