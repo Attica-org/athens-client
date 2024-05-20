d324137051ba3863198e746a9212fdcc0b2fc610
H-sooyeon <wndqhr__@naver.com>
2024-05-20 22:15:27 +0900

    [13] feat: sns 공유 및 클립보드 복사 추가

--- src/app/(chat)/_components/atoms/ShareButton.tsx
+++ src/app/(chat)/_components/atoms/ShareButton.tsx
@@ -1,15 +1,34 @@
 'use client';
 
 import ShareIcon from '@/assets/icons/ShareIcon';
-import { useRouter } from 'next/navigation';
-import React from 'react';
+import { useParams } from 'next/navigation';
+import React, { MouseEventHandler } from 'react';
 
-export default function ShareButton() {
-  const router = useRouter();
+type Props = {
+  title: string;
+};
 
-  const shareSNS = () => {
-    // TODO: SNS 공유 기능 추가
-    router.push('/agora/flow/end-agora');
+export default function ShareButton({ title }: Props) {
+  const pathname = useParams();
+
+  const shareSNS:MouseEventHandler<HTMLButtonElement> = async (e) => {
+    if (navigator.share) {
+      e.preventDefault();
+      if (navigator.canShare()) {
+        navigator.share({
+          title,
+          text: '다양한 사람들과 토론에 함께하세요!',
+          url: `${process.env.NEXT_PUBLIC_BASE_URL}/agoras/${pathname.agora}`,
+        });
+      }
+    } else {
+      try {
+        await navigator.clipboard.writeText(`${process.env.NEXT_PUBLIC_BASE_URL}/agoras/${pathname.agora}`);
+        alert('공유기능이 제공되지 않는 환경입니다.\n클립보드에 링크가 복사되었습니다.');
+      } catch (error) {
+        alert('공유와 복사가 제공되지 않는 환경입니다.');
+      }
+    }
   };
 
   return (
