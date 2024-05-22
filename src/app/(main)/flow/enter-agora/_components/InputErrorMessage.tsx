dc9eb43b6ad33d36f4c8d9dceb6fad77c4cea21c
H-sooyeon <wndqhr__@naver.com>
2024-05-22 20:57:47 +0900

    [#14] refactor: 사용자 닉네임 길이 제한 에러 메시지 분리

---
+++ src/app/(main)/flow/enter-agora/_components/InputErrorMessage.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { useEnter } from '@/store/enter';
+import React from 'react';
+import { useShallow } from 'zustand/react/shallow';
+
+export default function InputErrorMessage() {
+  const { message } = useEnter(useShallow((state) => ({ message: state.message })));
+
+  return (
+    <div>
+      { message && (
+        <div role="alert" aria-live="polite" className="mb-12 dark:text-dark-con-color text-red-400 text-sm w-full flex justify-center items-center">
+          {`** ${message} **`}
+        </div>
+      )}
+    </div>
+  );
+}
