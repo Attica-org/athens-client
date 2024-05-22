920576b1cc8f27bbcebdd1eb393decdd9ed8757b
H-sooyeon <wndqhr__@naver.com>
2024-05-22 21:01:19 +0900

    [#14] feat: 아고라 입장 모달 전역 상태 선언

---
+++ src/store/enter.ts
@@ -0,0 +1,61 @@
+import { create } from 'zustand';
+import { immer } from 'zustand/middleware/immer';
+
+type ProfileImage = {
+  id: number,
+  name: string,
+  file: string,
+};
+
+type Position = 'cons' | 'pros' | 'observers';
+
+interface EnterState {
+  nickname: string;
+  message: string;
+  selectedProfileImage: {
+    id: number,
+    name: string,
+    file: string,
+  };
+  selectedPosition: Position;
+  setMessage: (message: string) => void;
+  setNickname: (nickname: string) => void;
+  setProfileImage: (profileImage: ProfileImage) => void;
+  setSelectedPosition: (selectedPosition: Position) => void;
+  reset: () => void;
+}
+
+const initialState: EnterState = {
+  nickname: '',
+  message: '',
+  selectedProfileImage: {
+    id: 1,
+    name: '도끼 든 회색 곰',
+    file: 'bear.png',
+  },
+  selectedPosition: 'observers',
+  setMessage: () => {},
+  setSelectedPosition: () => {},
+  setNickname: () => {},
+  setProfileImage: () => {},
+  reset: () => {},
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export const useEnter = create(immer<EnterState>((set) => ({
+  ...initialState,
+  setMessage: (message: string) => set({ message }),
+  setSelectedPosition: (selectedPosition: Position) => set({ selectedPosition }),
+  setNickname: (nickname: string) => set({ nickname }),
+  setProfileImage: (profileImage: ProfileImage) => set({ selectedProfileImage: profileImage }),
+  reset: () => set({
+    nickname: '',
+    message: '',
+    selectedProfileImage: {
+      id: 1,
+      name: '도끼 든 회색 곰',
+      file: 'bear.png',
+    },
+    selectedPosition: 'observers',
+  }),
+})));
