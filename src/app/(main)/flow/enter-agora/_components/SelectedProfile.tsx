cb5548bfbe186d3ac6bc79fc5a93e6056b8ee116
H-sooyeon <wndqhr__@naver.com>
2024-05-22 20:58:18 +0900

    [#14] refactor: 사용자 닉네임, 프로필 이미지 선택 분리

---
+++ src/app/(main)/flow/enter-agora/_components/SelectedProfile.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import React, { ChangeEventHandler, useEffect, useRef } from 'react';
+import UserImage from '@/app/_components/atoms/UserImage';
+import { useEnter } from '@/store/enter';
+import { useShallow } from 'zustand/react/shallow';
+
+export default function SelectedProfile() {
+  const {
+    nickname, setNickname, profileImage,
+  } = useEnter(
+    useShallow((state) => ({
+      nickname: state.nickname,
+      setNickname: state.setNickname,
+      profileImage: state.selectedProfileImage,
+    })),
+  );
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const nicknameChange:ChangeEventHandler<HTMLInputElement> = (e) => {
+    setNickname(e.target.value);
+  };
+
+  inputRef.current?.focus();
+
+  useEffect(() => {
+    const handleOutSideClick = (e: MouseEvent) => {
+      if (inputRef.current && !inputRef.current.contains(e.target as Node)) {
+        inputRef.current.focus();
+      }
+    };
+
+    window.addEventListener('click', handleOutSideClick);
+
+    const cleanup = () => {
+      window.removeEventListener('click', handleOutSideClick);
+    };
+
+    return cleanup;
+  }, []);
+
+  return (
+    <div className="flex justify-start items-center mb-10">
+      <UserImage className="w-65 h-65 bg-white" name={profileImage.name} file={profileImage.file} w={65} h={65} />
+      <input ref={inputRef} aria-label="닉네임 입력창" type="text" value={nickname} placeholder={profileImage.name} onChange={nicknameChange} className="ml-12 text-sm dark:bg-dark-light-500 outline-none w-145 placeholder:text-sm p-3 rounded-md" />
+    </div>
+  );
+}
---
+++ src/app/(main)/flow/enter-agora/_components/SetUserProfile.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import React, { useEffect } from 'react';
+import UserImage from '@/app/_components/atoms/UserImage';
+import PROFLELIST from '@/constants/userProfileImage';
+import { useShallow } from 'zustand/react/shallow';
+import { useEnter } from '@/store/enter';
+import SelectedProfile from './SelectedProfile';
+
+type ProfileImageName = {
+  id: number;
+  name: string;
+  file: string;
+};
+
+export default function SetUserProfile() {
+  const {
+    setProfileImage, reset
+  } = useEnter(
+    useShallow((state) => ({
+      setProfileImage: state.setProfileImage,
+      reset: state.reset,
+    })),
+  );
+
+  const selectProfile = (profile: ProfileImageName):void => {
+    setProfileImage(profile);
+  };
+
+  const handleKeyDownSetProfile = (
+    e: React.KeyboardEvent<HTMLElement>,
+    profile: ProfileImageName,
+  ) => {
+    if (e.key === 'Enter') {
+      selectProfile(profile);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      reset();
+    };
+  }, [])
+
+  return (
+    <div className="flex flex-col justiy-start items-center">
+      <SelectedProfile />
+      <ul aria-label="사용할 프로필 이미지 선택" className="grid grid-cols-5 under-mobile:grid-cols-4 mobile:grid-cols-4 foldable:grid-cols-5 gap-y-5  pl-1rem">
+        {PROFLELIST.map((profileImageName) => (
+          <li
+            key={profileImageName.id}
+            className="cursor-pointer mr-5 w-fit flex justify-center items-center border-1 border-gray-300 rounded-full"
+          >
+            <div
+              role="button"
+              tabIndex={0}
+              aria-label={profileImageName.name}
+              onClick={() => selectProfile(profileImageName)}
+              onKeyDown={(e) => handleKeyDownSetProfile(e, profileImageName)}
+            >
+              <UserImage className="rounded-full w-45 h-45 bg-white" file={profileImageName.file} name={profileImageName.name} w={45} h={45} />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
