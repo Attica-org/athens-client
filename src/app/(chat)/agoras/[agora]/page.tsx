db65c343ea4031ba3521a91b1652fe36d7c3a964
H-sooyeon <wndqhr__@naver.com>
2024-05-26 03:03:35 +0900

    refactor: 알림 메시지 메시지 컴포넌트 안으로 이동

--- src/app/(chat)/agoras/[agora]/page.tsx
+++ src/app/(chat)/agoras/[agora]/page.tsx
@@ -1,15 +1,13 @@
 import Message from '@/app/(chat)/_components/molecules/Message';
 import React from 'react';
-import ChatNotification from '../../_components/atoms/ChatNotification';
 
 export default function Page() {
   return (
     <main
       aria-label="채팅"
-      className="flex flex-1 flex-col justify-between h-full"
+      className="flex flex-col justify-between h-full items-stretch"
     >
       <Message />
-      <ChatNotification />
     </main>
   );
 }
