3c50f6cdfeb68fdbd29d0c0f2725d8b5445acce3
H-sooyeon <wndqhr__@naver.com>
2024-05-26 22:40:58 +0900

    [#15] feat: 관찰자 선택 시 프로필 설정 불가 메시지 출력

--- src/app/(main)/flow/enter-agora/_components/ModalPosSelectContainer.tsx
+++ src/app/(main)/flow/enter-agora/_components/ModalPosSelectContainer.tsx
@@ -1,33 +1,44 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import ModalPosSelectBtn from '@/app/(main)/_components/atoms/ModalPosSelectBtn';
 import { useEnter } from '@/store/enter';
 import { useShallow } from 'zustand/react/shallow';
 
-type Position = 'cons' | 'pros' | 'observers';
+type Position = 'CONS' | 'PROS' | 'OBSERVERS';
 
 export default function ModalPosSelectContainer() {
-  const { selectedPosition, setSelectedPosition } = useEnter(
+  const {
+    selectedPosition, setSelectedPosition, setMessage,
+  } = useEnter(
     useShallow((state) => ({
+      setMessage: state.setMessage,
       selectedPosition: state.selectedPosition,
       setSelectedPosition: state.setSelectedPosition,
     })),
   );
+
+  useEffect(() => {
+    if (selectedPosition === 'OBSERVERS') {
+      setMessage('관찰자는 프로필을 설정할 수 없습니다.');
+    } else if (selectedPosition === 'CONS' || selectedPosition === 'PROS') {
+      setMessage('');
+    }
+  }, [selectedPosition, setMessage]);
 
   const selectPosition = (position: Position) => {
     setSelectedPosition(position);
   };
 
   return (
-    <div className="mt-2rem flex justify-center items-center text-sm under-mobile:text-xs min-w-200">
-      <ModalPosSelectBtn selectedPosition={selectedPosition} selectPosition={selectPosition} position="pros" color="blue">
+    <div className="mb-15 mt-8 flex justify-center items-center text-sm under-mobile:text-xs min-w-200">
+      <ModalPosSelectBtn selectedPosition={selectedPosition} selectPosition={selectPosition} position="PROS" color="blue">
         찬성
       </ModalPosSelectBtn>
-      <ModalPosSelectBtn selectedPosition={selectedPosition} selectPosition={selectPosition} position="cons" color="red">
+      <ModalPosSelectBtn selectedPosition={selectedPosition} selectPosition={selectPosition} position="CONS" color="red">
         반대
       </ModalPosSelectBtn>
-      <ModalPosSelectBtn selectedPosition={selectedPosition} selectPosition={selectPosition} position="observers" color="athens-main">
+      <ModalPosSelectBtn selectedPosition={selectedPosition} selectPosition={selectPosition} position="OBSERVERS" color="athens-main">
         관찰자
       </ModalPosSelectBtn>
       <span className="pl-6 text-xs">로 입장</span>
