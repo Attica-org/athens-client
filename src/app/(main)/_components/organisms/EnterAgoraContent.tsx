7ddd03f7990a3b4339604fbaa9ecb22c3be0a0a0
H-sooyeon <wndqhr__@naver.com>
2024-05-29 20:14:03 +0900

    feat: 아고라 입장하기 컴포너트로 분리

---
+++ src/app/(main)/_components/organisms/EnterAgoraContent.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { useAgora } from '@/store/agora';
+import tokenManager from '@/utils/tokenManager';
+import { useQuery } from '@tanstack/react-query';
+import React, { useEffect } from 'react';
+import Loading from '@/app/_components/atoms/loading';
+import { getAgoraTitle } from '../../_lib/getAgoraTitle';
+import ModalPosSelectContainer from '../../flow/enter-agora/_components/ModalPosSelectContainer';
+import InputErrorMessage from '../../flow/enter-agora/_components/InputErrorMessage';
+import SetUserProfile from '../../flow/enter-agora/_components/SetUserProfile';
+import EnterAgoraButton from '../../flow/enter-agora/_components/EnterAgoraButton';
+
+export default function EnterAgoraContent() {
+  const { selectedAgora, setSelectedAgora } = useAgora();
+  const redirectUrl: string | null = tokenManager.getRedirectUrl();
+  const agoraId = redirectUrl?.split('/').pop() || '-1';
+
+  const { data: title, isSuccess } = useQuery({
+    queryKey: ['agoraTitle', agoraId],
+    queryFn: getAgoraTitle,
+    enabled: !selectedAgora.id && agoraId !== '-1',
+  });
+
+  useEffect(() => {
+    if (isSuccess) {
+      setSelectedAgora({ id: Number(agoraId), title });
+      tokenManager.clearRedirectUrl();
+    }
+  }, [agoraId, title, setSelectedAgora, isSuccess]);
+
+  return (
+    <>
+      {selectedAgora.title
+        ? (<p className="text-base p-1rem pt-5 flex justify-center items-center text-center break-keep font-medium">{selectedAgora.title}</p>)
+        : (
+          <div className="mb-1rem flex justify-center items-center">
+            <p className="mr-8">
+              불러오는 중...
+            </p>
+            <Loading w="16" h="16" />
+          </div>
+        )}
+      <ModalPosSelectContainer />
+      <InputErrorMessage />
+      <SetUserProfile />
+      <EnterAgoraButton />
+    </>
+  );
+}
--- src/app/(main)/_components/templates/EnterAgora.tsx
+++ src/app/(main)/_components/templates/EnterAgora.tsx
@@ -1,25 +1,14 @@
-'use client';
-
-import React from 'react';
-import { useAgora } from '@/store/agora';
+import React, { Suspense } from 'react';
+import Loading from '@/app/_components/atoms/loading';
 import ModalBase from '../../../_components/molecules/ModalBase';
-import ModalPosSelectContainer from '../../flow/enter-agora/_components/ModalPosSelectContainer';
-import SetUserProfile from '../../flow/enter-agora/_components/SetUserProfile';
-import EnterAgoraButton from '../../flow/enter-agora/_components/EnterAgoraButton';
-import InputErrorMessage from '../../flow/enter-agora/_components/InputErrorMessage';
+import EnterAgoraContent from '../organisms/EnterAgoraContent';
 
 export default function EnterAgora() {
-  const { selectedAgora } = useAgora();
-
   return (
     <ModalBase title="아고라 입장" removeIcon animation={false}>
-      <p className="text-base p-1rem flex justify-center items-cener text-center break-keep font-medium">
-        {selectedAgora.title}
-      </p>
-      <ModalPosSelectContainer />
-      <InputErrorMessage />
-      <SetUserProfile />
-      <EnterAgoraButton />
+      <Suspense fallback={<Loading />}>
+        <EnterAgoraContent />
+      </Suspense>
     </ModalBase>
   );
 }
