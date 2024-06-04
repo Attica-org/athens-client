0352d1d6e612616a7912099a501ab83b9a476a02
H-sooyeon <wndqhr__@naver.com>
2024-06-04 22:10:24 +0900

    refactor: Agora 타입 제거 및 agoraId url에 바로 적용

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,17 +1,12 @@
 import type { MetadataRoute } from 'next';
 import { getAgoraList } from '../lib/getAgoras';
 
-type Agora = {
-  id: string;
-  updatedAt: string;
-};
-
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const agoras = await getAgoraList();
 
-  const urls: MetadataRoute.Sitemap = agoras.map((agora: Agora) => ({
-    url: `http://localhost:3000/agoras/${agora.id}`,
-    lastModified: new Date(agora.updatedAt),
+  const urls: MetadataRoute.Sitemap = agoras.map((agora: number) => ({
+    url: `http://localhost:3000/agoras/${agora}`,
+    lastModified: new Date(),
     changeFrequency: 'daily',
     priority: 1,
   }));
