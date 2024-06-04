616b0eba34a3915f0aa9bee8bd91e76f8bd67f10
H-sooyeon <wndqhr__@naver.com>
2024-06-04 22:07:58 +0900

    [#18] feat: fetchWrapper로 인증 에러 한 곳에서 처리

--- src/app/(main)/_lib/getAgoraTitle.ts
+++ src/app/(main)/_lib/getAgoraTitle.ts
@@ -1,12 +1,21 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/naming-convention */
+import fetchWrapper from '@/lib/fetchWrapper';
+import { getToken } from '@/lib/getToken';
+import tokenManager from '@/utils/tokenManager';
 import { QueryFunction } from '@tanstack/react-query';
 
 // eslint-disable-next-line import/prefer-default-export
 export const getAgoraTitle:QueryFunction<
 { title: string, status: string }, [_1: string, _2: string]> = async ({ queryKey }) => {
   const [agoraId, _] = queryKey;
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/agoraTitle`, {
+
+  // 토큰을 가지고 있는지 확인
+  if (tokenManager.getToken() === undefined) {
+    await getToken();
+  }
+
+  const res = await fetchWrapper.call('/api/v1/auth/title', {
     next: {
       tags: [`${agoraId}`, 'agoraTitle'],
     },
@@ -14,15 +23,16 @@ export const getAgoraTitle:QueryFunction<
     cache: 'no-cache',
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.API_TOKEN}`,
+      Authorization: `Bearer ${tokenManager.getToken()}`,
     },
   });
 
-  if (!res.ok) {
+  if (res.success === false) {
+    console.log(res.error.message);
     throw new Error('Network response was not ok');
   }
 
-  const result = res.json().then((data) => data.response);
+  const result = res.response;
 
   return result;
 };
