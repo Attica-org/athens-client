12ecb9b18a11aa67a266cf4fa7395e6e1be2b100
H-sooyeon <wndqhr__@naver.com>
2024-06-05 21:34:12 +0900

    feat: 종료된 아고라의 경우 message input창 미출력

--- src/app/(chat)/_components/molecules/MessageInput.tsx
+++ src/app/(chat)/_components/molecules/MessageInput.tsx
@@ -10,10 +10,12 @@ import { usePathname } from 'next/navigation';
 import tokenManager from '@/utils/tokenManager';
 import { InfiniteData, useQueryClient } from '@tanstack/react-query';
 import { Message } from '@/app/model/Message';
+import { useAgora } from '@/store/agora';
 
 export default function MessageInput() {
   const [message, setMessage] = useState<string>('');
   const [isComposing, setIsComposing] = useState(false);
+  const { enterAgora } = useAgora();
   const inputRef = useRef<HTMLTextAreaElement>(null);
   const { setGoDown } = useMessageStore();
   const agoraId = usePathname().split('/').pop() as string;
@@ -170,7 +172,7 @@ export default function MessageInput() {
       client.current.activate();
     };
 
-    if (tokenManager.getToken() !== undefined) {
+    if (tokenManager.getToken() !== undefined && enterAgora.status !== 'CLOSED') {
       connect();
     } else {
       console.error('Token is not found');
@@ -182,30 +184,32 @@ export default function MessageInput() {
   }, [agoraId]);
 
   return (
-    <section className="flex border-t-1 dark:border-dark-light-300 sticky bottom-0 right-0 left-0 w-full bg-white dark:bg-dark-light-300">
-      <form className="pl-1.5rem p-12 pb-0 flex flex-1 justify-start items-center">
-        <textarea
-          aria-label="보낼 메세지 입력창"
-          ref={inputRef}
-          value={message}
-          onChange={handleMessage}
-          onKeyDown={handleKeyDown}
-          onCompositionStart={handleCompositionStart}
-          onCompositionEnd={handleCompositionEnd}
-          placeholder="메시지 보내기"
-          className="placeholder:text-athens-gray-thick dark:placeholder:text-dark-light-400
+    enterAgora.status !== 'CLOSED' && (
+      <section className="flex border-t-1 dark:border-dark-light-300 sticky bottom-0 right-0 left-0 w-full bg-white dark:bg-dark-light-300">
+        <form className="pl-1.5rem p-12 pb-0 flex flex-1 justify-start items-center">
+          <textarea
+            aria-label="보낼 메세지 입력창"
+            ref={inputRef}
+            value={message}
+            onChange={handleMessage}
+            onKeyDown={handleKeyDown}
+            onCompositionStart={handleCompositionStart}
+            onCompositionEnd={handleCompositionEnd}
+            placeholder="메시지 보내기"
+            className="placeholder:text-athens-gray-thick dark:placeholder:text-dark-light-400
           dark:placeholder:text-opacity-85 dark:text-opacity-85 dark:text-white w-full text-base
           focus-visible:outline-none dark:bg-dark-light-300 resize-none overflow-hidden h-40"
-        />
-      </form>
-      <button
-        type="button"
-        onClick={sendMessage}
-        aria-label="메세지 보내기"
-        className="bg-athens-main pl-10 pr-10 cursor-pointer h-full"
-      >
-        <SendIcon className="w-30" fill="white" />
-      </button>
-    </section>
+          />
+        </form>
+        <button
+          type="button"
+          onClick={sendMessage}
+          aria-label="메세지 보내기"
+          className="bg-athens-main pl-10 pr-10 cursor-pointer h-full"
+        >
+          <SendIcon className="w-30" fill="white" />
+        </button>
+      </section>
+    )
   );
 }
