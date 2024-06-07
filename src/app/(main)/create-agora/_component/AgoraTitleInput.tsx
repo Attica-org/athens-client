57b27d999582ac70754235e16502729727e81e95
H-sooyeon <wndqhr__@naver.com>
2024-06-07 19:05:21 +0900

    feat: title 입력 전역 상태로 관리

--- src/app/(main)/create-agora/_component/AgoraTitleInput.tsx
+++ src/app/(main)/create-agora/_component/AgoraTitleInput.tsx
@@ -5,15 +5,13 @@ import React, { ChangeEventHandler, useState } from 'react';
 import { useShallow } from 'zustand/react/shallow';
 
 function AgoraTitleInput() {
-  const [input, setInput] = useState<string>('');
   const [message, setMessage] = useState<string | null>('주제를 입력해주세요.');
-  const { setTitle } = useCreateAgora(useShallow((state) => ({ setTitle: state.setTitle })));
+  const { title, setTitle } = useCreateAgora(useShallow((state) => ({
+    title: state.title, setTitle: state.setTitle,
+  })));
 
   const changeInput:ChangeEventHandler<HTMLInputElement> = (e) => {
-    setInput(() => {
-      setTitle(e.target.value);
-      return e.target.value;
-    });
+    setTitle(e.target.value);
 
     if (e.target.value.trim().length === 0) {
       setMessage('주제를 입력해주세요.');
@@ -29,7 +27,7 @@ function AgoraTitleInput() {
       <input
         aria-label="생성할 아고라 주제 입력창"
         type="text"
-        value={input}
+        value={title}
         onChange={changeInput}
         placeholder="토론 할 주제를 입력해주세요."
         className="placeholder:text-athens-gray-thick w-full p-0.5rem text-sm border-1 border-gray-300 rounded-md dark:bg-dark-light-300 dark:placeholder:text-white dark:placeholder:text-opacity-85 dark:border-0 dark:text-white"
