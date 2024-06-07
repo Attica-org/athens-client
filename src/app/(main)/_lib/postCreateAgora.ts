59f740e546abb815398e2ee5083f6a1ce00d241b
H-sooyeon <wndqhr__@naver.com>
2024-06-07 19:04:22 +0900

    [#19] feat: 아고라 생성하기 에러처리

--- src/app/(main)/_lib/postCreateAgora.ts
+++ src/app/(main)/_lib/postCreateAgora.ts
@@ -11,6 +11,11 @@ type Props = {
   duration: number | null,
 };
 
+const TITLE_NULL = '{"title":"공백일 수 없습니다"}';
+const CATEGORY_ERROR = '{"capacity":"1 이상이어야 합니다"}';
+const COLOR_NULL = '{"color":"공백일 수 없습니다"}';
+const CAPACITY_NULL = '{"categoryId":"널이어서는 안됩니다"}';
+
 // eslint-disable-next-line import/prefer-default-export
 export const postCreateAgora = async (info: Props) => {
   // 토큰을 가지고 있는지 확인
@@ -36,19 +41,25 @@ export const postCreateAgora = async (info: Props) => {
 
   if (res.success === false) {
     if (res.error.code === 1001) {
-      if (Number(info.category) < 1) {
+      if (Number(info.category) < 1 || res.error.message === CATEGORY_ERROR) {
         showToast('허용되지 않는 카테고리입니다.', 'error');
-      } else if (info.color === null) {
+      } else if (info.color === null || res.error.message === COLOR_NULL) {
         showToast('아고라 색상을 선택해주세요.', 'error');
-      } else if (info.capacity === null) {
+      } else if (info.capacity === null || res.error.message === CAPACITY_NULL) {
         showToast('카테고리를 선택해주세요.', 'error');
+      } else if (info.title.trim() === null || res.error.message === TITLE_NULL) {
+        showToast('아고라 제목을 입력해주세요.', 'error');
       } else {
         showToast('생성 실패했습니다.\n 다시 시도해주세요.', 'error');
       }
     }
+
+    return null;
   }
 
   const result = res.response;
+
+  console.log(res);
 
   return result;
 };
