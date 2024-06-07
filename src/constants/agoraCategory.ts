a634758090cce70040369b0709dc729b5ac3be1d
H-sooyeon <wndqhr__@naver.com>
2024-06-07 19:06:03 +0900

    feat: 카테고리 아이템-사회/복지 추가

--- src/constants/agoraCategory.ts
+++ src/constants/agoraCategory.ts
@@ -1,9 +1,10 @@
 const AGORACATEGORY = [
   { id: 0, innerText: '전체', value: '1' },
-  { id: 1, innerText: '지식/공부', value: '2' },
-  { id: 2, innerText: '문화/예술', value: '3' },
-  { id: 3, innerText: '음식/여행', value: '4' },
-  { id: 4, innerText: '일상/취미', value: '5' },
+  { id: 1, innerText: '사회/복지', value: '2' },
+  { id: 2, innerText: '지식/공부', value: '3' },
+  { id: 3, innerText: '문화/예술', value: '4' },
+  { id: 4, innerText: '음식/여행', value: '5' },
+  { id: 5, innerText: '일상/취미', value: '6' },
 ];
 
 export default AGORACATEGORY;
