3337d10fac25d70ad70789109fa33c19f9384b55
H-sooyeon <wndqhr__@naver.com>
2024-06-08 00:31:51 +0900

    refactor: 이벤트 함수 아이콘 컴포넌트로 이동

--- src/app/_components/atoms/BackButton.tsx
+++ src/app/_components/atoms/BackButton.tsx
@@ -1,19 +1,12 @@
 'use client';
 
 import BackIcon from '@/assets/icons/BackIcon';
-import { useRouter } from 'next/navigation';
 import React from 'react';
 
 export default function BackButton() {
-  const router = useRouter();
-
-  const handleBack = () => {
-    router.replace('/home');
-  };
-
   return (
     <button aria-label="뒤로가기" type="button">
-      <BackIcon onClick={handleBack} className="w-22 ml-1rem cursor-pointer" />
+      <BackIcon className="w-22 ml-1rem cursor-pointer" />
     </button>
   );
 }
