b677c747fc6e47a8a86c0579d79f101194fb6da4
H-sooyeon <wndqhr__@naver.com>
2024-06-11 20:39:01 +0900

    [#19] feat: 닉네임 중복 체크 추가

--- src/app/(main)/_lib/postEnterAgoraInfo.ts
+++ src/app/(main)/_lib/postEnterAgoraInfo.ts
@@ -14,6 +14,14 @@ type Props = {
   agoraId: number,
 };
 
+const ALREADY_PARTICIPATED = 'User has already participated';
+const NICKNAME_DUPLICATED = 'The nickname is already in use';
+
+const splitMessage = (message: string) => {
+  const split = message.split('.');
+  return split[0];
+};
+
 // eslint-disable-next-line import/prefer-default-export
 export const postEnterAgoraInfo = async ({ info, agoraId }: Props) => {
   // 토큰을 가지고 있는지 확인
@@ -30,7 +38,7 @@ export const postEnterAgoraInfo = async ({ info, agoraId }: Props) => {
     credentials: 'include',
     body: JSON.stringify({
       nickname: info.nickname ? info.nickname : '',
-      photo_num: info.id ? info.id : 1,
+      photoNum: info.id ? info.id : 1,
       type: info.role,
       voteType: null,
     }),
@@ -48,7 +56,11 @@ export const postEnterAgoraInfo = async ({ info, agoraId }: Props) => {
         showToast('입장 실패했습니다.\n 다시 시도해주세요.', 'error');
       }
     } else if (res.error.code === 1004) {
-      showToast('이미 참여한 아고라입니다.', 'error');
+      if (splitMessage(res.error.message) === ALREADY_PARTICIPATED) {
+        showToast('이미 참여한 아고라입니다.', 'error');
+      } else if (splitMessage(res.error.message) === NICKNAME_DUPLICATED) {
+        showToast('닉네임이 중복됩니다.', 'error');
+      }
     } else if (res.error.code === 2000) {
       showToast('선택한 타입의 인원이 꽉 찼습니다.', 'error');
     }
