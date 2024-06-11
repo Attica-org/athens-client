2080ae49425b09868fb9185c7b422611872a5120
H-sooyeon <wndqhr__@naver.com>
2024-06-11 20:37:21 +0900

    [#19] fix: 토론 시작/종료 응답 결과 toast로 출력

--- src/app/(chat)/_components/molecules/DiscussionStatus.tsx
+++ src/app/(chat)/_components/molecules/DiscussionStatus.tsx
@@ -1,57 +1,61 @@
 'use client';
 
 import { useMutation } from '@tanstack/react-query';
-import React, { Suspense, useState } from 'react';
-import { usePathname } from 'next/navigation';
+import React, { Suspense } from 'react';
 import EyeIcon from '@/assets/icons/EyeIcon';
 import { useChatInfo } from '@/store/chatInfo';
 import { useAgora } from '@/store/agora';
 import Loading from '@/app/_components/atoms/loading';
 import { AgoraMeta } from '@/app/model/AgoraMeta';
 import { useShallow } from 'zustand/react/shallow';
+import toast from 'react-hot-toast';
 import { patchAgoraStart } from '../../_lib/patchAgoraStart';
 import { patchAgoraEnd } from '../../_lib/patchAgoraEnd';
 import DiscussionTimer from '../atoms/DiscussionTimer';
 import VoteResult from '../atoms/VoteResult';
+import showToast from '@/utils/showToast';
 
 type Props = {
   meta: AgoraMeta | undefined;
 };
 
 export default function DiscussionStatus({ meta }: Props) {
-  const [progress, setProgress] = useState(false);
-  const agoraId = usePathname().split('/').pop() as string;
   const { enterAgora } = useAgora();
-  const { endVoteCount } = useChatInfo(useShallow((state) => ({
+  const { endVoteCount, start } = useChatInfo(useShallow((state) => ({
     endVoteCount: state.endVoteCount,
+    start: state.start,
   })));
 
   const agoraStart = useMutation({
-    mutationFn: async () => patchAgoraStart(agoraId as string),
+    mutationFn: async () => patchAgoraStart(enterAgora.id),
     onMutate: () => {
-      setProgress((prev) => !prev);
     },
     onSuccess: async (response) => {
       // 타이머 시작
-      const setStart = useChatInfo.getState().setDiscussionStart;
-      setStart(response.startTime);
-      console.log('아고라 시작!', response.startTime);
+      if (response) {
+        toast.success('토론이 시작되었습니다.');
+      } else {
+        showToast('토론 시작에 실패했습니다.', 'error');
+      }
     },
     onError: () => {
-      setProgress((prev) => !prev);
       // console.dir(error);
       // alert('문제가 발생했습니다. 다시 시도해주세요.');
     },
   });
 
   const agoraEnd = useMutation({
-    mutationFn: async () => patchAgoraEnd(agoraId as string),
+    mutationFn: async () => patchAgoraEnd(enterAgora.id),
     onMutate: () => {
       // END 버튼을 누른 인원이 2/3 이상이면 종료
       // router.push(`/agoras/${agoraId}/flow/end-agora`);
     },
     onSuccess: async (response) => {
-      console.log('아고라 종료!', response);
+      if (response) {
+        toast.success('토론 종료에 투표하였습니다.');
+      } else {
+        toast.error('토론 종료 투표에 실패했습니다.');
+      }
     },
     onError: () => {
       // console.dir(error);
@@ -60,27 +64,22 @@ export default function DiscussionStatus({ meta }: Props) {
   });
 
   const toggleProgress = () => {
-    if (progress) {
-      const { setIsClosed } = useChatInfo.getState();
-      setIsClosed(true);
-      // 종료 api 호출
-      agoraEnd.mutate();
-    } else {
+    if (!start) {
       // 시작 api 호출
       agoraStart.mutate();
     }
   };
 
   return (
-    enterAgora.status !== 'closed' ? (
+    enterAgora.status !== 'CLOSED' ? (
       <>
         <button
           type="button"
           onClick={toggleProgress}
-          aria-label={`토론 ${progress ? '종료하기' : '시작하기'}`}
-          className={`italic ${progress ? 'bg-athens-main' : 'bg-red-500'} p-4 pl-15 pr-15 under-mobile:pl-10 under-mobile:pr-10 rounded-lg text-white mr-0.5rem`}
+          aria-label={`토론 ${start ? '종료하기' : '시작하기'}`}
+          className={`italic ${start ? 'bg-athens-main' : 'bg-red-500'} p-4 pl-15 pr-15 under-mobile:pl-10 under-mobile:pr-10 rounded-lg text-white mr-0.5rem`}
         >
-          {progress ? 'END' : 'START'}
+          {start ? 'END' : 'START'}
         </button>
         <div
           role="group"
@@ -88,9 +87,7 @@ export default function DiscussionStatus({ meta }: Props) {
           className="flex justify-center items-center"
         >
           <DiscussionTimer
-            duration={meta?.agora.duration || 60}
-            start={progress}
-            agoraId={meta?.agora.id || 0}
+            duration={meta?.agora.duration || 0}
           />
           <div
             role="status"
@@ -105,7 +102,13 @@ export default function DiscussionStatus({ meta }: Props) {
             className="flex justify-center items-center"
           >
             <EyeIcon className="w-1rem" />
-            <span className="pl-5 text-xs text-athens-gray-thick dark:text-white dark:text-opacity-85">{meta?.participants[2].count}</span>
+            {meta?.participants.map((participant) => (
+              participant.type === 'OBSERVER' && (
+                <span className="pl-5 text-xs text-athens-gray-thick dark:text-white dark:text-opacity-85">
+                  {participant.count || 0}
+                </span>
+              )
+            ))}
           </div>
         </div>
       </>
