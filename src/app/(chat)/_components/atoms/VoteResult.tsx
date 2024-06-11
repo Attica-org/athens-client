2d176b6bbdd02ab6e0e9076658db74456b943bc3
H-sooyeon <wndqhr__@naver.com>
2024-06-11 20:31:55 +0900

    fix: 투표 종료 후 받은 데이터로 화면에 출력

--- src/app/(chat)/_components/atoms/VoteResult.tsx
+++ src/app/(chat)/_components/atoms/VoteResult.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useAgora } from '@/store/agora';
+import { useVoteStore } from '@/store/vote';
 import { getVoteResult } from '../../_lib/getVoteResult';
 
 type Props = {
@@ -8,22 +9,21 @@ type Props = {
 };
 
 export default function VoteResult({ agoraId }: Props) {
+  const result = useVoteStore((state) => state.voteResult);
   const { enterAgora } = useAgora();
   const { data } = useQuery({
     queryKey: ['agora', `${agoraId}`, 'closed'],
     queryFn: getVoteResult,
-    enabled: enterAgora.status !== 'closed',
+    enabled: enterAgora.status !== 'CLOSED',
   });
 
   return (
-    { data } && (
     <div className="text-sm">
       최종 투표 결과 |
       <span className="dark:text-dark-pro-color text-blue-400"> 찬성 </span>
-      {data?.prosCount}
-      <span className="dark:text-dark-con-color text-red-400"> 반대 </span>
-      {data?.consCount}
+      {data?.prosCount || result.prosCount || 0}
+      <span className="dark:text-dark-con-color text-red-400 ml-10"> 반대 </span>
+      {data?.consCount || result.consCount || 0}
     </div>
-    )
   );
 }
