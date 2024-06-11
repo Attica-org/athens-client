66f9ab1df0bd6f040c4688ba7ab1a8419c1966f0
H-sooyeon <wndqhr__@naver.com>
2024-06-11 20:35:38 +0900

    [#19] fix: 투표 결과 갱신 시 화면 출력으로 수정

--- src/app/(chat)/_components/templates/ResultAgora.tsx
+++ src/app/(chat)/_components/templates/ResultAgora.tsx
@@ -4,15 +4,25 @@ import ModalBase from '@/app/_components/molecules/ModalBase';
 import { useVoteStore } from '@/store/vote';
 import Confetti from '@/utils/Confetti';
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useShallow } from 'zustand/react/shallow';
 
 export default function ResultAgora() {
   const prosColor = 'bg-blue-400';
   const consColor = 'bg-red-400';
-  const { voteResult } = useVoteStore(useShallow((state) => ({
+  const { voteResult, setVoteEnd, reset } = useVoteStore(useShallow((state) => ({
     voteResult: state.voteResult,
+    setVoteEnd: state.setVoteEnd,
+    reset: state.reset,
   })));
+
+  useEffect(() => {
+    setVoteEnd(false);
+    return () => {
+      reset();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
@@ -25,11 +35,23 @@ export default function ResultAgora() {
           >
             기후 변화 대책에 대한 토론
           </h2>
-          <div
-            aria-label="더 많은 표를 얻은 측"
-            className={`${voteResult.consCount > voteResult.prosCount ? consColor : prosColor} mt-1.5rem text-white p-5 pl-1.5rem pr-1.5rem text-sm rounded-lg`}
-          >
-            {voteResult.consCount > voteResult.prosCount ? `반대 ${voteResult.consCount}` : `찬성 ${voteResult.prosCount}`}
+          <div className="flex justify-center items-center">
+            <div
+              aria-label="찬성 표 수"
+              className={`${prosColor} mt-1.5rem text-white p-5 pl-1.5rem pr-1.5rem text-sm rounded-lg mr-16`}
+            >
+              찬성
+              {' '}
+              {voteResult.prosCount || 0}
+            </div>
+            <div
+              aria-label="반대 표 수"
+              className={`${consColor} mt-1.5rem text-white p-5 pl-1.5rem pr-1.5rem text-sm rounded-lg`}
+            >
+              반대
+              {' '}
+              {voteResult.consCount || 0}
+            </div>
           </div>
           <Link
             aria-label="홈으로 가기"
