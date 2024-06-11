574b8a5917d76cae8f5ddef55fdbcaea8d4261e3
H-sooyeon <wndqhr__@naver.com>
2024-06-11 20:40:13 +0900

    refactor: 사용하지 않는 serviceworker 미호출

--- src/app/_components/utils/ServiceWorkerRegistration.tsx
+++ src/app/_components/utils/ServiceWorkerRegistration.tsx
@@ -3,17 +3,17 @@
 import { useEffect } from 'react';
 
 export default function ServiceWorkerRegistration() {
-  useEffect(() => {
-    if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/sw.js').then((registration) => {
-          console.log('SW registered: ', registration);
-        }).catch((registrationError) => {
-          console.log('SW registration failed: ', registrationError);
-        });
-      });
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+  //     window.addEventListener('load', () => {
+  //       navigator.serviceWorker.register('/sw.js').then((registration) => {
+  //         console.log('SW registered: ', registration);
+  //       }).catch((registrationError) => {
+  //         console.log('SW registration failed: ', registrationError);
+  //       });
+  //     });
+  //   }
+  // }, []);
 
   useEffect(() => {
     if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
