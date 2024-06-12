d0935465aed27d9b96f73fb6d99308bd59d124b9
H-sooyeon <wndqhr__@naver.com>
2024-06-12 23:49:48 +0900

    feat: 투표 후 얻은 데이터를 우선

--- src/app/(chat)/_components/atoms/VoteResult.tsx
+++ src/app/(chat)/_components/atoms/VoteResult.tsx
@@ -1,5 +1,7 @@
+'use client';
+
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useAgora } from '@/store/agora';
 import { useVoteStore } from '@/store/vote';
 import { getVoteResult } from '../../_lib/getVoteResult';
@@ -11,19 +13,24 @@ type Props = {
 export default function VoteResult({ agoraId }: Props) {
   const result = useVoteStore((state) => state.voteResult);
   const { enterAgora } = useAgora();
-  const { data } = useQuery({
+  const { data, refetch } = useQuery({
     queryKey: ['agora', `${agoraId}`, 'closed'],
     queryFn: getVoteResult,
-    enabled: enterAgora.status !== 'CLOSED',
   });
+
+  useEffect(() => {
+    if (enterAgora.status === 'CLOSED') {
+      refetch();
+    }
+  }, [enterAgora.status, refetch]);
 
   return (
     <div className="text-sm">
       최종 투표 결과 |
       <span className="dark:text-dark-pro-color text-blue-400"> 찬성 </span>
-      {data?.prosCount || result.prosCount || 0}
+      { result.prosCount || data?.prosCount || 0}
       <span className="dark:text-dark-con-color text-red-400 ml-10"> 반대 </span>
-      {data?.consCount || result.consCount || 0}
+      {result.consCount || data?.consCount || 0}
     </div>
   );
 }
