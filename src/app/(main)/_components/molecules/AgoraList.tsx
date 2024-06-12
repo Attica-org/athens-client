9ff29cb3e76ff32d5a0972cd2b96a1995eee44f2
H-sooyeon <wndqhr__@naver.com>
2024-06-12 23:55:09 +0900

    feat: 아고라 없을 때 보이는 메시지 grid 적용 안되게 외부로 이동

--- src/app/(main)/_components/molecules/AgoraList.tsx
+++ src/app/(main)/_components/molecules/AgoraList.tsx
@@ -40,17 +40,19 @@ export default function AgoraList({ searchParams }: Props) {
 
   return (
     <>
-      <div className="grid under-large:grid-cols-5 gap-x-1rem gap-y-1rem under-mobile:grid-cols-2 mobile:grid-cols-2 foldable:grid-cols-3 tablet:grid-cols-4 under-tablet:grid-cols-4 xl:grid-cols-6 sm:grid-cols-3 lg:grid-cols-5 under-xl:grid-cols-4">
-        {data?.pages[0].agoras.length < 1
-          ? <NoAgoraMessage />
-          : data?.pages.map((page) => (
-            <React.Fragment key={page.agoras[0]?.id}>
-              {page.agoras.map((agora) => (
-                <Agora key={agora.id} agora={agora} />
-              ))}
-            </React.Fragment>
-          ))}
-      </div>
+      {data?.pages[0].agoras.length < 1
+        ? <NoAgoraMessage />
+        : (
+          <div className="grid under-large:grid-cols-5 gap-x-1rem gap-y-1rem under-mobile:grid-cols-2 mobile:grid-cols-2 foldable:grid-cols-3 tablet:grid-cols-4 under-tablet:grid-cols-4 xl:grid-cols-6 sm:grid-cols-3 lg:grid-cols-5 under-xl:grid-cols-4">
+            {data?.pages.map((page) => (
+              <React.Fragment key={page.agoras[0]?.id}>
+                {page.agoras.map((agora) => (
+                  <Agora key={agora.id} agora={agora} />
+                ))}
+              </React.Fragment>
+            ))}
+          </div>
+        )}
       <div ref={ref} />
     </>
   );
