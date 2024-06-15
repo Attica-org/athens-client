38e10fbf2777a3d4be08afb3352871668b40e6eb
H-sooyeon <wndqhr__@naver.com>
2024-06-15 23:44:05 +0900

    fix: 토큰 재발급 시 헤더 미포함

--- src/lib/getReissuanceToken.ts
+++ src/lib/getReissuanceToken.ts
@@ -11,10 +11,6 @@ export const getReissuanceToken = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/auth/reissue`, {
     method: 'POST',
     credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${tokenManager.getToken()}`,
-    },
   });
 
   if (!res.ok) {
