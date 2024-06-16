b4d6d6d209243e4df9b6c84cbca9a5f92b7c2e3d
H-sooyeon <wndqhr__@naver.com>
2024-06-16 14:59:23 +0900

    [#20] 공유 기능 미제공 환경일 시 bottom sheet 제공

--- src/app/(chat)/_components/atoms/ShareButton.tsx
+++ src/app/(chat)/_components/atoms/ShareButton.tsx
@@ -3,7 +3,10 @@
 import ShareIcon from '@/assets/icons/ShareIcon';
 import showToast from '@/utils/showToast';
 import { useParams } from 'next/navigation';
-import React, { MouseEventHandler } from 'react';
+import React, { MouseEventHandler, useState } from 'react';
+import { BottomSheet } from 'react-spring-bottom-sheet';
+
+import 'react-spring-bottom-sheet/dist/style.css';
 
 type Props = {
   title: string;
@@ -11,28 +14,31 @@ type Props = {
 
 export default function ShareButton({ title }: Props) {
   const pathname = useParams();
+  const [open, setOpen] = useState(false);
 
   const shareSNS:MouseEventHandler<HTMLButtonElement> = async () => {
     const url = `${process.env.NEXT_PUBLIC_CLIENT_URL}/agoras/${pathname.agora}`;
 
-    if (navigator.share) {
+    if (window.navigator.share) {
       try {
-        await navigator.share({
+        await window.navigator.share({
           title,
           text: '다양한 사람들과 토론에 함께하세요!',
           url,
         });
       } catch (error) {
-        showToast('공유기능을 불러오는데 실패했습니다.', 'error');
-      }
-    } else {
-      try {
         await navigator.clipboard.writeText(url);
         showToast('공유기능 미지원 환경으로, 클립보드에 링크가 복사되었습니다.', 'error');
-      } catch (error) {
-        showToast('클립보드 복사에 실패했습니다.', 'error');
       }
+    } else if (window.innerWidth < 1024) {
+      setOpen(true);
+    } else {
+      // 모달로 공유하기 창 띄우기
     }
+  };
+
+  const handleDismiss = () => {
+    setOpen(false);
   };
 
   return (
@@ -43,6 +49,9 @@ export default function ShareButton({ title }: Props) {
       className="cursor-pointer"
     >
       <ShareIcon className="w-17 mr-1rem under-mobile:mr-0.5rem" />
+      <BottomSheet onDismiss={handleDismiss} open={open}>
+        SNS 공유하기
+      </BottomSheet>
     </button>
   );
 }
