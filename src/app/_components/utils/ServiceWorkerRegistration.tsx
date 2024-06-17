8f9d4215279bd2e9f9eb3641ab56586b535de1b2
H-sooyeon <wndqhr__@naver.com>
2024-06-17 14:54:02 +0900

    [#19] fix: custom 서비스 워커가 아닌 다른 워커 적용시 custom으로 변경

--- src/app/_components/utils/ServiceWorkerRegistration.tsx
+++ src/app/_components/utils/ServiceWorkerRegistration.tsx
@@ -1,32 +1,49 @@
 'use client';
 
+import showToast from '@/utils/showToast';
 import { useEffect } from 'react';
 
 export default function ServiceWorkerRegistration() {
-  // useEffect(() => {
-  //   if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-  //     window.addEventListener('load', () => {
-  //       navigator.serviceWorker.register('/sw.js').then((registration) => {
-  //         console.log('SW registered: ', registration);
-  //       }).catch((registrationError) => {
-  //         console.log('SW registration failed: ', registrationError);
-  //       });
-  //     });
-  //   }
-  // }, []);
-
   useEffect(() => {
-    if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/custom-sw.js').then(() => {
-          // registration
-          // console.log('Custom SW registered: ', registration);
-        }).catch(() => {
-          // registrationError
-          // console.log('SW registration failed: ', registrationError);
+    const getRegisterServiceWorker = async () => {
+      try {
+        const registration = await navigator.serviceWorker.getRegistrations();
+
+        const customSwUrl = '/custom-sw.js';
+        let needsRegistration = true;
+
+        registration.some((regist) => {
+          if (regist.active && regist.active.scriptURL.includes(customSwUrl)) {
+            needsRegistration = false;
+            // console.log('Custom SW already registered: ', regist);
+            return true;
+          }
+          return false;
         });
-      });
-    }
+
+        if (needsRegistration) {
+          if (typeof window !== 'undefined' && 'serviceWorker' in navigator) {
+            // console.log('ServiceWorkerRegistration 실행');
+            try {
+              navigator.serviceWorker.register('/custom-sw.js').then(() => {
+                // registration
+                // console.log('Custom SW registered: ', regist);
+              }).catch(() => {
+                // registrationError
+                // console.log('SW registration failed: ', registrationError);
+              });
+            } catch (error) {
+              // console.log('error', error);
+            }
+          }
+        }
+      } catch (registrationError) {
+        showToast('Service Worker 등록에 실패했습니다. 잠시 후 다시 시도해주세요.', 'error');
+        // console.log('registration failed: ', registrationError);
+      }
+    };
+
+    getRegisterServiceWorker();
   }, []);
 
   return null;
