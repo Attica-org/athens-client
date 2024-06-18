6003e4d70517befe6a519add087e03c0024f428d
H-sooyeon <wndqhr__@naver.com>
2024-06-18 16:54:57 +0900

    feat: 토큰 발급 retry 횟수 지정

--- src/lib/getReissuanceToken.ts
+++ src/lib/getReissuanceToken.ts
@@ -1,35 +1,56 @@
 import showToast from '@/utils/showToast';
 import tokenManager from '@/utils/tokenManager';
+import getKey from '@/utils/getKey';
 import { getToken } from './getToken';
+
+let retry = 3;
 
 // eslint-disable-next-line import/prefer-default-export
 export const getReissuanceToken = async () => {
+  const key = await getKey();
+
   if (!tokenManager.getToken()) {
     await getToken();
   }
 
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/auth/reissue`, {
+  const res = await fetch(`${key.BASE_URL}/api/v1/auth/reissue`, {
     method: 'POST',
     credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${tokenManager.getToken()}`,
+    },
   });
 
   if (!res.ok) {
     const result = await res.json();
     if (result.error.code === 1003 || result.error.code === 1202) {
-      await getToken();
-      await getReissuanceToken();
-    } else if (result.error.code === 1201) {
-      if (result.error.message === 'Invalid JWT signature.' || result.error.message === 'Unsupported JWT token.') {
+      if(retry > 0) {
         await getToken();
         await getReissuanceToken();
+        retry--;
+      }
+    } else if (result.error.code === 1201) {
+      if (result.error.message === 'Invalid JWT signature.' || result.error.message === 'Unsupported JWT token.') {
+        if(retry > 0) {
+          retry--;
+          await getToken();
+          await getReissuanceToken();
+        }
       }
     } else {
       showToast('인증 오류가 발생했습니다.', 'error');
     }
 
+    if(retry <= 0) {
+      showToast('인증 오류가 발생했습니다.', 'error');
+      retry = 3;
+    }
+
     return;
   }
 
+  retry = 3;
   const result = await res.json();
 
   tokenManager.setToken(result.accessToken);
--- src/lib/getToken.ts
+++ src/lib/getToken.ts
@@ -1,10 +1,18 @@
+import getKey from '@/utils/getKey';
 import showToast from '@/utils/showToast';
 import tokenManager from '@/utils/tokenManager';
 
+let retry = 3;
+
 // eslint-disable-next-line import/prefer-default-export
 export const getToken = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/v1/temp-user`, {
+  const key = await getKey();
+
+  const res = await fetch(`${key.BASE_URL}/api/v1/temp-user`, {
     method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
     credentials: 'include',
   });
 
@@ -12,16 +20,27 @@ export const getToken = async () => {
     const result = await res.json();
     if (result.error.code === 1201 || result.error.code === 1002 || result.error.code === 1202) {
       if (result.error.message === 'Invalid JWT signature.' || result.error.message === 'Unsupported JWT token.') {
-        await getToken();
+        if(retry > 0) {
+          retry--;
+          await getToken();
+        }
       }
     } else {
       showToast('인증 오류가 발생했습니다.', 'error');
     }
 
+    if(retry <= 0) {
+      showToast('인증 오류가 발생했습니다.', 'error');
+      retry = 3;
+    }
+
     return;
   }
 
+  retry = 3;
   const result = await res.json();
+
+  console.log('accessToken 발급: ', result.response.accessToken);
 
   tokenManager.setToken(result.response.accessToken);
 };
