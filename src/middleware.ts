4c610b0d77cd126db01a2a7558e6e4a2acb481b5
H-sooyeon <wndqhr__@naver.com>
2024-06-19 01:07:05 +0900

    fix: 공유url 접근시 404 뜨는 문제 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,25 @@
 // middleware.ts
 import { NextRequest, NextResponse } from 'next/server';
+import tokenManager from './utils/tokenManager';
 
 // eslint-disable-next-line import/prefer-default-export
 export function middleware(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   requestHeaders.set('x-pathname', request.nextUrl.pathname);
+
+  // 공유를 통해 들어온 유저는 referer가 없기 때문에
+  // referer가 없으며 nextUrl.pathname이 agoras로 시작하면 토큰이 있는지 확인하고 없으면 입장하기 모달 페이지로 이동
+  if (request.headers.get('referer') === null && request.nextUrl.pathname.split('/')[1] === 'agoras') {
+    const refererPathname = request.nextUrl.pathname;
+    const agoraId = refererPathname.split('/')[2];
+
+    const token = tokenManager.getToken();
+
+    if (!token) {
+      tokenManager.setRedirectUrl(refererPathname);
+      return NextResponse.redirect(`${process.env.NEXT_CLIENT_URL}/flow/enter-agora/${agoraId}`);
+    }
+  }
 
   return NextResponse.next({
     request: {
