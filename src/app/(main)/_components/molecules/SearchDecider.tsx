f9ecc7fc8b4924bdd4af64e3228606a1adabe016
DodoJBNU <dh723@jbnu.ac.kr>
2024-06-20 17:31:07 +0900

    Fix : ErrorBoundary 래핑시 렌더링 중 컴포넌트 정의 문제 해결

--- src/app/(main)/_components/molecules/SearchDecider.tsx
+++ src/app/(main)/_components/molecules/SearchDecider.tsx
@@ -2,29 +2,43 @@
 
 import React, { Suspense } from 'react';
 import dynamic from 'next/dynamic';
+import { ErrorBoundary, FallbackProps } from 'react-error-boundary';
+import ErrorFallback from '@/app/_components/templates/ErrorFallback';
 import Loading from '../atoms/loading';
 
 const SearchAgoraList = dynamic(() => import('./SearchAgoraList'));
 const AgoraList = dynamic(() => import('./AgoraList'));
 
 type Props = {
-  searchParams: { status?: string, category?: string, q?: string }
+  searchParams: { status?: string; category?: string; q?: string };
 };
 
+const errorFallbackProps = {
+  headerLabel: '아고라 목록을 불러오던 중 오류가 발생했습니다.',
+  btnLabel: '다시 불러오기',
+};
+
+function FallbackComponent(props: FallbackProps) {
+  return <ErrorFallback {...props} {...errorFallbackProps} />;
+}
 export default function SearchDecider({ searchParams }: Props) {
   const { q } = searchParams;
 
   if (q) {
     return (
-      <Suspense fallback={<Loading />}>
-        <SearchAgoraList searchParams={searchParams} />
-      </Suspense>
+      <ErrorBoundary FallbackComponent={FallbackComponent}>
+        <Suspense fallback={<Loading />}>
+          <SearchAgoraList searchParams={searchParams} />
+        </Suspense>
+      </ErrorBoundary>
     );
   }
 
   return (
-    <Suspense fallback={<Loading />}>
-      <AgoraList searchParams={searchParams} />
-    </Suspense>
+    <ErrorBoundary FallbackComponent={FallbackComponent}>
+      <Suspense fallback={<Loading />}>
+        <AgoraList searchParams={searchParams} />
+      </Suspense>
+    </ErrorBoundary>
   );
 }
