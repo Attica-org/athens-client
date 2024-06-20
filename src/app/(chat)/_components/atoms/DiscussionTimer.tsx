78c6f6cd265bc23f330702ce3eac6ac08f859243
H-sooyeon <wndqhr__@naver.com>
2024-06-20 19:06:06 +0900

    [#26] feat: 아고라 타임아웃 후 종료api 호출

--- src/app/(chat)/_components/atoms/DiscussionTimer.tsx
+++ src/app/(chat)/_components/atoms/DiscussionTimer.tsx
@@ -5,10 +5,10 @@ import { useRouter } from 'next/navigation';
 import React, { useEffect } from 'react';
 import { useShallow } from 'zustand/react/shallow';
 import useTimer from '@/hooks/useTimer';
-// import { useMutation } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 import { useAgora } from '@/store/agora';
-// import showToast from '@/utils/showToast';
-// import { patchAgoraTimeOut } from '../../_lib/patchAgoraTimeOut';
+import showToast from '@/utils/showToast';
+import { patchAgoraTimeOut } from '../../_lib/patchAgoraTimeOut';
 
 type Props = {
   duration: number;
@@ -16,39 +16,43 @@ type Props = {
 
 export default function DiscussionTimer({ duration }: Props) {
   const startTime = useChatInfo.getState().start;
-  const { start, end } = useChatInfo(useShallow((state) => ({
-    start: state.start,
-    end: state.end,
-  })));
-  const { formattedTime, isFinished, resetTimer } = useTimer(startTime, duration);
+  const { start, end } = useChatInfo(
+    useShallow((state) => ({
+      start: state.start,
+      end: state.end,
+    })),
+  );
+  const { formattedTime, isFinished, resetTimer } = useTimer(
+    startTime,
+    duration,
+  );
   const { enterAgora, setEnterAgora } = useAgora();
   const router = useRouter();
 
-  // const agoraEnd = useMutation({
-  //   mutationFn: async () => patchAgoraTimeOut(enterAgora.id),
-  //   onMutate: () => {
-  //   },
-  //   onSuccess: async (response) => {
-  //     if (response) {
-  //       router.push(`/agoras/${enterAgora.id}/flow/end-agora`);
-  //     } else {
-  //       showToast('데이터 연결에 실패했습니다.', 'error');
-  //     }
-  //   },
-  //   onError: () => {
-  //     // console.dir(error);
-  //     // alert('문제가 발생했습니다. 다시 시도해주세요.');
-  //   },
-  // });
+  const agoraEnd = useMutation({
+    mutationFn: async () => patchAgoraTimeOut(enterAgora.id),
+    retry: 2,
+    onMutate: () => {},
+    onSuccess: async (response) => {
+      if (response) {
+        router.push(`/agoras/${enterAgora.id}/flow/end-agora`);
+      } else {
+        showToast('데이터 연결에 실패했습니다.', 'error');
+      }
+    },
+    onError: () => {
+      // console.dir(error);
+      // alert('문제가 발생했습니다. 다시 시도해주세요.');
+    },
+  });
 
   useEffect(() => {
     if ((isFinished && start) || end) {
       resetTimer();
-      // agoraEnd.mutate();
+      agoraEnd.mutate();
       setEnterAgora({ ...enterAgora, status: 'CLOSED' });
-      router.push(`/agoras/${enterAgora.id}/flow/end-agora`);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isFinished, enterAgora.id, router, start, resetTimer, end]);
 
   return (
