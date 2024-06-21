946b064476ba9594215dadea6d14502e80bca49d
H-sooyeon <wndqhr__@naver.com>
2024-06-21 20:47:26 +0900

    [#35] refactor: 종료된 아고라는 알림 메시지 미출력

--- src/app/(chat)/_components/atoms/ChatNotification.tsx
+++ src/app/(chat)/_components/atoms/ChatNotification.tsx
@@ -1,36 +1,39 @@
 'use client';
 
-import { useChatInfo } from '@/store/chatInfo';
+import { useAgora } from '@/store/agora';
 import React, { useEffect, useState } from 'react';
 import { useShallow } from 'zustand/react/shallow';
 
 export default function ChatNotification() {
   const [showMessage, setShowMessage] = useState(false);
-  const { end } = useChatInfo(useShallow((state) => ({
-    end: state.end,
-  })));
+  const { enterAgora } = useAgora(
+    useShallow((state) => ({
+      enterAgora: state.enterAgora,
+    })),
+  );
 
   useEffect(() => {
-    if (end) return () => {};
+    if (enterAgora.status !== 'CLOSED') return () => {};
     const timer = setTimeout(() => {
       setShowMessage(true);
     }, 10000);
 
     return () => clearTimeout(timer);
-  }, [end]);
+  }, [enterAgora.status]);
 
   return (
-    !showMessage && !end && (
-    <div className="flex p-0.5rem pl-1rem pr-1rem">
-      <div
-        role="alert"
-        aria-live="polite"
-        className="rounded-lg text-center flex flex-col justify-center items-center text-xs lg:text-sm under-mobile:text-xs text-athens-gray-thick p-11 bg-athens-gray dark:bg-dark-light-200 dark:text-dark-line w-full break-keep"
-      >
-        사용자들간의 쾌적한 토론 환경을 위해 바른말을 사용해주세요.
-        <div>토론을 시작하려면 상단의 START 버튼을 눌러주세요.</div>
+    !showMessage &&
+    !enterAgora.status && (
+      <div className="flex p-0.5rem pl-1rem pr-1rem">
+        <div
+          role="alert"
+          aria-live="polite"
+          className="rounded-lg text-center flex flex-col justify-center items-center text-xs lg:text-sm under-mobile:text-xs text-athens-gray-thick p-11 bg-athens-gray dark:bg-dark-light-200 dark:text-dark-line w-full break-keep"
+        >
+          사용자들간의 쾌적한 토론 환경을 위해 바른말을 사용해주세요.
+          <div>토론을 시작하려면 상단의 START 버튼을 눌러주세요.</div>
+        </div>
       </div>
-    </div>
     )
   );
 }
