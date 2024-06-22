807f9b1d3973bbff030fcf2c28e55a76321b643c
H-sooyeon <wndqhr__@naver.com>
2024-06-22 23:21:38 +0900

    design: 채팅방 레이아웃에 fixed 적용

--- src/app/(chat)/layout.tsx
+++ src/app/(chat)/layout.tsx
@@ -9,16 +9,14 @@ type Props = {
 
 export default function Layout({ children }: Props) {
   return (
-    <div className="w-full overflow-y-hidden overflow-x-hidden h-full flex justify-center items-center xl:w-[1580px]">
-      <section className="w-full flex flex-1 h-full max-lg:pb-3rem min-w-270 flex-grow max-width-screen relative">
-        <section className="flex flex-1 h-full flex-col w-full">
-          <div className="overflow-y-hidden justify-center items-stretch flex flex-col w-full h-full flex-1 relative">
-            <section className="flex sticky w-full top-0 bg-white dark:bg-dark-bg-light justify-between items-center pt-10 min-w-270 border-b-1 border-gray-200 dark:border-dark-light-300">
-              <Header />
-            </section>
-            {children}
-            <MessageInput />
-          </div>
+    <div className="w-full overflow-y-hidden overflow-x-hidden h-full flex justify-center items-center xl:w-[1580px] relative">
+      <section className="w-full h-full max-lg:pb-3rem min-w-270 max-width-screen fixed">
+        <section className="overflow-y-hidden h-full flex-col w-full relative">
+          <section className="sticky w-full top-0 bg-white dark:bg-dark-bg-light pt-10 min-w-270 border-b-1 border-gray-200 dark:border-dark-light-300">
+            <Header />
+          </section>
+          {children}
+          <MessageInput />
         </section>
         <AgoraUserSide />
       </section>
