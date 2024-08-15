19a77e39b82d4b1bb67814f91ea1357ed0354784
H-sooyeon <wndqhr__@naver.com>
2024-08-15 18:02:33 +0900

    refactor: 캐시 사용하여 반복되는 서버 요청 방지, experiment

--- next.config.js
+++ next.config.js
@@ -8,6 +8,28 @@ const nextConfig = {
     });
     return config;
   },
+  headers: async () => {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, no-cache, must-revalidate',
+          },
+        ],
+      },
+      { headers: [
+        {
+          key: 'Cache-Control',
+          value: 'public, max-age=86400, immutable',
+        },
+      ],
+
+      source: '/:path(.+\\.(?:ico|png|svg|jpg|jpeg|gif|webp|json|mp3|mp4|ttf|ttc|otf|woff|woff2)$)',
+    }
+    ];
+  }
 };
 
 const withPWA = require('next-pwa')({
