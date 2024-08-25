25c3cca656524a48cbf2ec13a954eaf2b64a7c7a
H-sooyeon <wndqhr__@naver.com>
2024-08-25 21:59:56 +0900

    [#80] feat: 종료된 아고라 투표 결과 그래프로 표현

--- src/app/(main)/_components/atoms/CategoryAgora.tsx
+++ src/app/(main)/_components/atoms/CategoryAgora.tsx
@@ -2,18 +2,19 @@
 
 import { AgoraData } from '@/app/model/Agora';
 import { useAgora } from '@/store/agora';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter } from 'next/navigation';
 import React, { useEffect, useState } from 'react';
 import COLOR from '@/constants/agoraColor';
 import isActiveAgora from '@/utils/isActiveAgora';
+import ClosedAgoraVoteResultBar from './ClosedAgoraVoteResultBar';
 
 type Props = {
   agora: AgoraData;
+  className?: string;
 };
 
-export default function CategoryAgora({ agora }: Props) {
+export default function CategoryAgora({ agora, className }: Props) {
   const router = useRouter();
-  const searchParams = useSearchParams();
   const { setSelectedAgora, setEnterAgora } = useAgora();
   const [selectedColor, setSelectedColor] = useState(COLOR[0].value);
 
@@ -38,7 +39,8 @@ export default function CategoryAgora({ agora }: Props) {
       status: agora.status,
     });
 
-    const AgoraStatus = searchParams.get('status') || 'active';
+    const AgoraStatus =
+      agora.status === ('RUNNING' || 'QUEUED') ? 'active' : 'closed';
 
     if (AgoraStatus === 'active') {
       router.push(`/flow/enter-agora/${agora.id}`);
@@ -59,7 +61,7 @@ export default function CategoryAgora({ agora }: Props) {
   return (
     <article
       id={`${agora.id}`}
-      className="w-165 under-mobile:w-130 p-10 border-1 rounded-lg flex flex-col justify-center items-center dark:bg-dark-light-300 dark:border-gray-500"
+      className={`${className} w-165 under-mobile:w-130 p-10 border-1 rounded-lg flex flex-col justify-center items-center dark:bg-dark-light-300 dark:border-gray-500`}
     >
       <div
         className={`${selectedColor} under-mobile:w-3rem under-mobile:h-3rem w-4rem h-4rem rounded-3xl under-mobile:rounded-2xl relative`}
@@ -70,40 +72,53 @@ export default function CategoryAgora({ agora }: Props) {
           />
         )}
       </div>
-      <h2 className="text-xs under-mobile:text-xs under-mobile:font-semibold pt-10 dark:text-white break-all w-full text-center">
+      <h3 className="text-xs under-mobile:text-xs under-mobile:font-semibold pt-10 dark:text-white break-all w-full text-center">
         {agora.agoraTitle}
-      </h2>
-      <p aria-label="아고라 참여 인원" className="text-xs pt-7">
-        <span className="pr-5 text-athens-gray-thick text-nowrap dark:text-dark-line">
-          <span className="text-blue-600 pr-3 dark:text-dark-pro-color">
-            찬성
-          </span>
-          {isActiveAgora(agora) ? agora.participants.pros : agora.prosCount}명
-        </span>
-        <span className="pr-5 text-athens-gray-thick text-nowrap dark:text-dark-line">
-          <span className="text-red-600 pr-3 dark:text-dark-con-color">
-            반대
-          </span>
-          {isActiveAgora(agora) ? agora.participants.cons : agora.consCount}명
-        </span>
-        {isActiveAgora(agora) && (
-          <span className="under-mobile:bloc break-keep">
-            <span className="pr-3 dark:text-white dark:text-opacity-85">
-              관찰자
+      </h3>
+      <div
+        role="group"
+        aria-label={
+          agora.status === 'CLOSED' ? '투표 결과' : '아고라 참여 인원'
+        }
+        className="text-xs pt-7 w-full"
+      >
+        {isActiveAgora(agora) ? (
+          <>
+            <span className="pr-5 text-athens-gray-thick text-nowrap dark:text-dark-line">
+              <span className="text-blue-600 pr-3 dark:text-dark-pro-color">
+                찬성
+              </span>
+              {agora.participants.pros}명
             </span>
-            <span className="text-athens-gray-thick dark:text-dark-line">
-              {agora.participants.observer}명
+            <span className="pr-5 text-athens-gray-thick text-nowrap dark:text-dark-line">
+              <span className="text-red-600 pr-3 dark:text-dark-con-color">
+                반대
+              </span>
+              {agora.participants.cons}명
             </span>
-          </span>
+            <span className="under-mobile:bloc break-keep">
+              <span className="pr-3 dark:text-white dark:text-opacity-85">
+                관찰자
+              </span>
+              <span className="text-athens-gray-thick dark:text-dark-line">
+                {agora.participants.observer}명
+              </span>
+            </span>
+          </>
+        ) : (
+          <ClosedAgoraVoteResultBar
+            prosPercentage={agora.prosCount}
+            consPercentage={agora.consCount}
+          />
         )}
-      </p>
+      </div>
       <button
         aria-label="아고라 입장하기"
         onClick={enterAgora}
         type="button"
         className="text-sm under-mobile:text-xs text-white bg-athens-main p-4 pt-5 pb-5 mt-10 w-9rem under-mobile:w-110 rounded-md"
       >
-        {searchParams.get('status') === 'closed' ? '결과보기' : '입장하기'}
+        {agora.status === 'CLOSED' ? '결과보기' : '입장하기'}
       </button>
     </article>
   );
