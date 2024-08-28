6b21133ba2f8af10e3cbea1a9fd95c4de2a57409
DOdoJBNU <dh723@jbnu.ac.kr>
2024-08-29 01:58:16 +0900

    Feat : 이모지 전송 가능 상태 확인 반환 함수

---
+++ src/hooks/useIsEmojiSendable.ts
@@ -0,0 +1,25 @@
+'use client';
+
+import { useChatInfo } from '@/store/chatInfo';
+import { useAgora } from '@/store/agora';
+import { useShallow } from 'zustand/react/shallow';
+
+export default function useIsEmojiSendable() {
+  // end 상태가 있으면 이모지 전송 불가
+  // status가 종료되었다면 이모지 전송 불가
+  const { end } = useChatInfo(
+    useShallow((state) => ({
+      end: state.end,
+    })),
+  );
+  const { status } = useAgora((state) => ({
+    status: state.enterAgora.status,
+  }));
+  if (end) {
+    return false;
+  }
+  if (status === 'CLOSED') {
+    return false;
+  }
+  return true;
+}
