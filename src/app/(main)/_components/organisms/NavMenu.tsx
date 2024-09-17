ba908294107f83d2c627f0283eedd1f40476b90e
H-sooyeon <wndqhr__@naver.com>
2024-09-17 11:41:02 +0900

    [#86] design: 최소 너비 고정

--- src/app/(main)/_components/organisms/NavMenu.tsx
+++ src/app/(main)/_components/organisms/NavMenu.tsx
@@ -29,7 +29,7 @@ export default function SideNav() {
           </div>
         </div>
       </div>
-      <div className="block lg:hidden w-full fixed bottom-0rem bg-white z-10 dark:bg-dark-light-200">
+      <div className="block lg:hidden w-full min-w-300 fixed bottom-0rem bg-white z-10 dark:bg-dark-light-200">
         <div className="w-lg flex flex-row justify-center items-center">
           <NavLinks />
         </div>
