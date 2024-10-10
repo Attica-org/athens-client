60601d984332fe61ddf4b71749cfcc84f58d40fc
H-sooyeon <wndqhr__@naver.com>
2024-10-10 23:14:01 +0900

    [#37] feat: 스크롤을 최하단으로 내리는 버튼 추가

--- src/assets/icons/ArrowDownIcon.tsx
+++ src/assets/icons/ArrowDownIcon.tsx
@@ -1,18 +1,23 @@
+'use client';
+
+import { useDarkMode } from '@/store/darkMode';
 import React from 'react';
 
 type Props = {
-  className: string;
+  className?: string;
 };
 
 export default function ArrowDownIcon({ className }: Props) {
+  const { darkMode } = useDarkMode();
+
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       className={className}
       viewBox="0 -960 960 960"
-      fill="#e8eaed"
+      fill={darkMode ? '#e8eaed' : '#000'}
     >
-      <path d="M480-344 240-584l56-56 184 184 184-184 56 56-240 240Z" />
+      <path d="M480-371.69 267.69-584 296-612.31l184 184 184-184L692.31-584 480-371.69Z" />
     </svg>
   );
 }
