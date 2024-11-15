67cdcc6c7c2f284ed697ab3f3e1d382f14ceda4e
H-sooyeon <wndqhr__@naver.com>
2024-11-15 21:06:46 +0900

    refactor: 인증 로직 변경으로 middleware에서의 enter-agora 세션 확인 제거

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,40 +1,15 @@
 // middleware.ts
-import { NextRequest, NextResponse } from 'next/server';
-import tokenManager from './utils/tokenManager';
+import { NextResponse } from 'next/server';
 import { getSession } from './serverActions/auth';
 
-export async function middleware(request: NextRequest) {
+export async function middleware() {
   const session = await getSession();
 
   if (!session) {
     return NextResponse.redirect(`${process.env.NEXT_CLIENT_URL}/`);
   }
 
-  const requestHeaders = new Headers(request.headers);
-  requestHeaders.set('x-pathname', request.nextUrl.pathname);
-  // 공유를 통해 들어온 유저는 referer가 없기 때문에
-  // referer가 없으며 nextUrl.pathname이 agoras로 시작하면 토큰이 있는지 확인하고 없으면 입장하기 모달 페이지로 이동
-  if (
-    request.headers.get('referer') === null &&
-    request.nextUrl.pathname.split('/')[1] === 'agoras'
-  ) {
-    const refererPathname = request.nextUrl.pathname;
-    const agoraId = refererPathname.split('/')[2];
-
-    const token = tokenManager.getToken();
-
-    if (!token) {
-      return NextResponse.redirect(
-        `${process.env.NEXT_CLIENT_URL}/flow/enter-agora/${agoraId}`,
-      );
-    }
-  }
-
-  return NextResponse.next({
-    request: {
-      headers: requestHeaders,
-    },
-  });
+  return NextResponse.next();
 }
 
 export const config = {
