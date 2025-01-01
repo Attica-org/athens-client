fd9a327a40b1a42edd4feb6b1d486dc94332da20
H-sooyeon <wndqhr__@naver.com>
2025-01-01 22:29:14 +0900

    fix: 키워드 검색 페이지에서 아고라 생성 페이지로 이동시 카테고리 버튼 리스트 안보이는 문제 수정

--- src/app/(main)/_components/organisms/CategoryButtonContainer.tsx
+++ src/app/(main)/_components/organisms/CategoryButtonContainer.tsx
@@ -3,17 +3,20 @@
 import React, { Suspense } from 'react';
 import { useSearchStore } from '@/store/search';
 import { useShallow } from 'zustand/react/shallow';
+import { usePathname } from 'next/navigation';
+import { createAgoraSegmentKey } from '@/constants/segmentKey';
 import CategoryButtonList from '../molecules/CategoryButtonList';
 import CategoryButtonListSkeleton from '../atoms/CategoryButtonListSkeleton';
 
 export default function CategoryButtonContainer() {
+  const pathname = usePathname();
   const { search } = useSearchStore(
     useShallow((state) => ({
       search: state.search,
     })),
   );
 
-  if (!search) {
+  if (!search || pathname === createAgoraSegmentKey) {
     return (
       <Suspense fallback={<CategoryButtonListSkeleton />}>
         <CategoryButtonList />
