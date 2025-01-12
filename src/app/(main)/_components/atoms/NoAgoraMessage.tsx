dbfdf9d7270d284f098465fe28706eb272f2697d
H-sooyeon <wndqhr__@naver.com>
2025-01-12 22:47:16 +0900

    refactor: 홈 접근성 표시 정보 추가

--- src/app/(main)/_components/atoms/NoAgoraMessage.tsx
+++ src/app/(main)/_components/atoms/NoAgoraMessage.tsx
@@ -3,7 +3,10 @@ import React from 'react';
 export default function NoAgoraMessage() {
   return (
     <div className="flex flex-col items-center justify-start pt-12 pb-32">
-      <p className="text-gray-500 dark:text-dark-line-light text-sm">
+      <p
+        aria-label="출력된 텍스트"
+        className="text-gray-500 dark:text-dark-line-light text-sm"
+      >
         아직 등록된 아고라가 없습니다.
       </p>
     </div>
