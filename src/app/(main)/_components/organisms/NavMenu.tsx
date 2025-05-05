56c320b8e69a56876327f5b2b2ead97e1aa6f7df
DOdoJBNU <dh723@jbnu.ac.kr>
2025-05-05 21:45:59 +0900

    refactor: 페이지 로드시, 스크린 리더가 간략한 페이지 설명을 읽도록 수정

--- src/app/(main)/_components/organisms/NavMenu.tsx
+++ src/app/(main)/_components/organisms/NavMenu.tsx
@@ -5,6 +5,9 @@ import DesktopNav from '../templates/DesktopNav';
 function SideNav() {
   return (
     <nav className="lg:h-dvh flex-1 max-w-12rem flex-grow">
+      <p className="sr-only" role="status">
+        해당 페이지에서 아고라를 검색하거나 입장할 수 있습니다.
+      </p>
       <DesktopNav />
       <MobileNav />
     </nav>
