09863c1cf4e7a3cfac33571428c305be7c82786e
DodoJBNU <dh723@jbnu.ac.kr>
2025-05-08 17:04:25 +0900

    refactor: 아고라 세부정보 대사 수정

--- src/app/(main)/_components/atoms/CategoryAgora.tsx
+++ src/app/(main)/_components/atoms/CategoryAgora.tsx
@@ -137,8 +137,13 @@ function CategoryAgora({ agora, className }: Props) {
       const { pros, cons, observer } = agoraData.participants;
       baseStr += `현재 찬성자는 ${pros}명, 반대자는 ${cons}명, 관찰자는 ${observer} 명입니다.`;
     } else {
-      const { prosCount, consCount } = agoraData;
-      baseStr += `토론에 참여했던 찬성자는 ${prosCount}명, 반대자는 ${consCount}명 입니다.`;
+      const { prosCount, consCount, totalMember } = agoraData;
+      const prosWidth =
+        prosCount <= 0 ? 0 : Math.floor((prosCount / totalMember) * 100);
+      const consWidth =
+        consCount <= 0 ? 0 : Math.floor((consCount / totalMember) * 100);
+
+      baseStr += `토론에 참여했던 인원은 ${totalMember}명이며, 찬성 ${prosWidth}%, 반대 ${consWidth}% 로 종료되었습니다.`;
     }
 
     baseStr += '입장하시겠습니까?';
