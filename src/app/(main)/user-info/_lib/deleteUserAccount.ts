e7a35628cc7154421b89fc84e435b12334591853
DodoJBNU <dh723@jbnu.ac.kr>
2025-05-30 15:08:36 +0900

    refactor: 사용자 정보 페이지 API Response 타입 수정

--- src/app/(main)/user-info/_lib/deleteUserAccount.ts
+++ src/app/(main)/user-info/_lib/deleteUserAccount.ts
@@ -1,8 +1,16 @@
+import { AgoraMemberInfo } from '@/app/model/AgoraMeta';
 import { SIGNIN_REQUIRED } from '@/constants/authErrorMessage';
 import { DELETE_USER_ERROR_MESSAGE } from '@/constants/responseErrorMessage';
 import { callFetchWrapper } from '@/lib/fetchWrapper';
 import { getSession } from '@/serverActions/auth';
 import isNull from '@/utils/validation/validateIsNull';
+
+type DeleteUserResponse = {
+  memberId: AgoraMemberInfo['memberId'];
+  isDeleted: true;
+  deletedAt: string;
+  deletedBy: string;
+};
 
 const deleteUserAccount = async () => {
   const session = await getSession();
@@ -11,14 +19,17 @@ const deleteUserAccount = async () => {
     throw new Error(SIGNIN_REQUIRED);
   }
 
-  const res = await callFetchWrapper<any>('/api/v1/auth/member', {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${session.user?.accessToken}`,
+  const res = await callFetchWrapper<DeleteUserResponse>(
+    '/api/v1/auth/member',
+    {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${session.user?.accessToken}`,
+      },
+      credentials: 'include',
     },
-    credentials: 'include',
-  });
+  );
 
   if (!res.ok && !res.success) {
     if (!res.error) {
