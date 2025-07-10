38b39cfab65c89e31fa49f719ff528707abff829
H-sooyeon <wndqhr__@naver.com>
2025-07-10 22:26:44 +0900

    fix: 이미지 업로드 후 크롭되지 않는 문제 수정
    README의 개발 기간 수정

--- src/app/_components/molecules/ImageCropper.tsx
+++ src/app/_components/molecules/ImageCropper.tsx
@@ -31,17 +31,13 @@ export default function ImageCropper() {
     })),
   );
 
-  const { loading, handleImgCrop } = useHandleImgCrop({
-    imgRef,
-    canvasRef,
-    setCropedPreview,
-  });
+  const { loading, handleImgCrop } = useHandleImgCrop(setCropedPreview);
 
-  const cropUserCustomImage = () => {
+  const cropUserCustomImage = useCallback(() => {
     if (!isNull(makeCrop)) {
-      handleImgCrop(makeCrop);
+      handleImgCrop({ makeCrop, imgRef, canvasRef });
     }
-  };
+  }, [makeCrop, handleImgCrop]);
 
   const handleKeyDownImgCrop = useCallback(
     (e: KeyboardEvent<HTMLButtonElement>) => {
@@ -56,75 +52,74 @@ export default function ImageCropper() {
     setOpacity('opacity-100');
   }, []);
 
-  if (loading) {
-    return (
-      <div className="min-w-300 w-full h-full flex absolute justify-center items-center z-50 top-0 right-0 left-0 bottom-0 bg-opacity-50 bg-dark-bg-dark">
-        <Loading
-          w="20"
-          h="20"
-          className="m-2 flex justify-center items-center"
-        />
-      </div>
-    );
-  }
-
   return (
-    <div
-      ref={dialogRef}
-      id="agora-profile-image-dialog"
-      role="dialog"
-      aria-modal="true"
-      aria-label="이미지 자르기 창"
-      aria-describedby="이미지를 원하는 영역으로 자르고 완료 버튼을 눌러주세요."
-      className="flex flex-col z-50 fixed top-0 left-0 w-full h-full dark:bg-dark-bg-dark bg-dark-line-light"
-    >
-      <main
-        className={`${opacity} transition duration-300 transform scale-100 h-full w-full`}
+    <>
+      <div
+        ref={dialogRef}
+        id="agora-profile-image-dialog"
+        role="dialog"
+        aria-modal="true"
+        aria-label="이미지 자르기 창"
+        aria-describedby="이미지를 원하는 영역으로 자르고 완료 버튼을 눌러주세요."
+        className="flex flex-col z-50 fixed top-0 left-0 w-full h-full dark:bg-dark-bg-dark bg-dark-line-light"
       >
-        <ImageCropperHeader
-          handleImgCrop={cropUserCustomImage}
-          handleKeyDownImgCrop={handleKeyDownImgCrop}
-        />
-        <section
-          aria-label="이미지 영역"
-          className="w-full h-full flex justify-center items-center pb-25"
+        <main
+          className={`${opacity} transition duration-300 transform scale-100 h-full w-full`}
         >
-          <ReactCrop
-            crop={crop}
-            aspect={1}
-            keepSelection
-            ruleOfThirds
-            onChange={(newCrop) => setCrop(newCrop)}
-            onComplete={(newCrop) => setMakeCrop(newCrop)}
-            className="w-fit h-fit"
-            aria-label="이미지 자르기"
+          <ImageCropperHeader
+            handleImgCrop={cropUserCustomImage}
+            handleKeyDownImgCrop={handleKeyDownImgCrop}
+          />
+          <section
+            aria-label="이미지 영역"
+            className="w-full h-full flex justify-center items-center pb-25"
           >
-            {!isNull(uploadImage.dataUrl) && (
-              <RenderMedia
-                file={uploadImage}
-                imgRef={imgRef}
-                setCrop={setCrop}
+            <ReactCrop
+              crop={crop}
+              aspect={1}
+              keepSelection
+              ruleOfThirds
+              onChange={(newCrop) => setCrop(newCrop)}
+              onComplete={(newCrop) => setMakeCrop(newCrop)}
+              className="w-fit h-fit"
+              aria-label="이미지 자르기"
+            >
+              {!isNull(uploadImage.dataUrl) && (
+                <RenderMedia
+                  file={uploadImage}
+                  imgRef={imgRef}
+                  setCrop={setCrop}
+                />
+              )}
+            </ReactCrop>
+          </section>
+          <div>
+            {crop && (
+              <canvas
+                aria-hidden
+                ref={canvasRef}
+                className="mt-12"
+                style={{
+                  display: 'none',
+                  border: '1px solid #000',
+                  objectFit: 'contain',
+                  width: Math.round(crop.width ?? 0),
+                  height: Math.round(crop.height ?? 0),
+                }}
               />
             )}
-          </ReactCrop>
-        </section>
-        <div>
-          {crop && (
-            <canvas
-              aria-hidden
-              ref={canvasRef}
-              className="mt-12"
-              style={{
-                display: 'none',
-                border: '1px solid #000',
-                objectFit: 'contain',
-                width: Math.round(crop.width ?? 0),
-                height: Math.round(crop.height ?? 0),
-              }}
-            />
-          )}
+          </div>
+        </main>
+      </div>
+      {loading && (
+        <div className="min-w-300 w-full h-full flex absolute justify-center items-center z-50 top-0 right-0 left-0 bottom-0 bg-opacity-50 bg-dark-bg-dark">
+          <Loading
+            w="20"
+            h="20"
+            className="m-2 flex justify-center items-center"
+          />
         </div>
-      </main>
-    </div>
+      )}
+    </>
   );
 }
